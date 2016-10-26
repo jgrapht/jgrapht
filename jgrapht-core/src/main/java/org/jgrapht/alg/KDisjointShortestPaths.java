7c8ff7383cd96ab56aaa73b00a8cc32db3342830
mizrachi <assaf.mizrachi@ecitele.com>
2016-10-26 15:30:21 +0300

    Check style fixes (replaced tabs with spaces, added missing @param).

--- jgrapht-core/src/main/java/org/jgrapht/alg/KDisjointShortestPaths.java
+++ jgrapht-core/src/main/java/org/jgrapht/alg/KDisjointShortestPaths.java
@@ -62,8 +62,8 @@ import org.jgrapht.graph.GraphWalk;
  * <ol>
  * <li> Create vertices V_in, V_out, U_in and U_out
  * <li> Create edges:
- * 	<ol>
- * 	<li> {@literal V_out-->U_in}, weight = W;
+ *     <ol>
+ *     <li> {@literal V_out-->U_in}, weight = W;
  *  <li> {@literal U_out-->V_in}, weight = W;
  *  <li> {@literal V_in-->V_out}, weight = 0;
  *  <li> {@literal U_in-->U_out}, weight = 0;
@@ -96,249 +96,252 @@ import org.jgrapht.graph.GraphWalk;
  *
  * @author Assaf Mizrachi
  * @since September 11, 2016
+ * 
+ * @param <V> Vertex
+ * @param <E> Edge
  */
 public class KDisjointShortestPaths<V, E> {
-	/**
-	 * Graph on which shortest paths are searched.
-	 */
-	private Graph<V, E> graph;
+    /**
+     * Graph on which shortest paths are searched.
+     */
+    private Graph<V, E> graph;
 
-	private List<List<E>> pathList;
+    private List<List<E>> pathList;
 
-	private int nPaths;
+    private int nPaths;
 
-	private V startVertex;
+    private V startVertex;
 
-	/**
-	 * Creates an object to calculate k disjoint shortest paths between the start
-	 * vertex and others vertices.
-	 *
-	 * @param graph
-	 *            graph on which shortest paths are searched. Note: graph will
-	 *            be modified (edges and weights) during the path computation.
-	 * @param startVertex
-	 *            start vertex of the calculated paths.
-	 * @param nPaths
-	 *            number of disjoint paths between the start vertex and an end
-	 *            vertex.
-	 *
-	 * @throws NullPointerException
-	 *             if the specified graph or startVertex is <code>null</code>.
-	 * @throws IllegalArgumentException
-	 *             if nPaths is negative or 0.
-	 */
-	public KDisjointShortestPaths(Graph<V, E> graph, V startVertex, int nPaths) {
-		assertKShortestPathsFinder(graph, startVertex, nPaths);
+    /**
+     * Creates an object to calculate k disjoint shortest paths between the start
+     * vertex and others vertices.
+     *
+     * @param graph
+     *            graph on which shortest paths are searched. Note: graph will
+     *            be modified (edges and weights) during the path computation.
+     * @param startVertex
+     *            start vertex of the calculated paths.
+     * @param nPaths
+     *            number of disjoint paths between the start vertex and an end
+     *            vertex.
+     *
+     * @throws NullPointerException
+     *             if the specified graph or startVertex is <code>null</code>.
+     * @throws IllegalArgumentException
+     *             if nPaths is negative or 0.
+     */
+    public KDisjointShortestPaths(Graph<V, E> graph, V startVertex, int nPaths) {
+        assertKShortestPathsFinder(graph, startVertex, nPaths);
 
-		this.graph = graph;
-		this.startVertex = startVertex;
-		this.nPaths = nPaths;
-	}
+        this.graph = graph;
+        this.startVertex = startVertex;
+        this.nPaths = nPaths;
+    }
 
-	/**
-	 * Returns the k shortest simple paths from start vertex to end vertex
-	 * in increasing order of weight.
-	 *
-	 * @param endVertex
-	 *            target vertex of the calculated disjoint paths.
-	 *
-	 * @return list of paths, or <code>null</code> if no path exists between the
-	 *         start vertex and the end vertex.
-	 */
-	public List<GraphPath<V, E>> getPaths(V endVertex) {
-		assertGetPaths(endVertex);
-		//ensure we have directed weighted graph
-		assert(this.graph instanceof DirectedGraph<?, ?>);
-		this.graph = new AsWeightedDirectedGraph<V, E>((DirectedGraph<V, E>) graph, new HashMap<>());
+    /**
+     * Returns the k shortest simple paths from start vertex to end vertex
+     * in increasing order of weight.
+     *
+     * @param endVertex
+     *            target vertex of the calculated disjoint paths.
+     *
+     * @return list of paths, or <code>null</code> if no path exists between the
+     *         start vertex and the end vertex.
+     */
+    public List<GraphPath<V, E>> getPaths(V endVertex) {
+        assertGetPaths(endVertex);
+        //ensure we have directed weighted graph
+        assert(this.graph instanceof DirectedGraph<?, ?>);
+        this.graph = new AsWeightedDirectedGraph<V, E>((DirectedGraph<V, E>) graph, new HashMap<>());
 
-		List<E> currentPath;
-		this.pathList = new ArrayList<>();
-		BellmanFordShortestPath<V, E> bellmanFordShortestPath;
-		int cPaths = 1;
-		do {
-			setUp(cPaths);
-			bellmanFordShortestPath = new BellmanFordShortestPath<V, E>(this.graph, this.startVertex);
-			currentPath = bellmanFordShortestPath.getPathEdgeList(endVertex);
-			if (currentPath != null) {
-				cPaths++;				
-				pathList.add(currentPath);
-			}			
-		} while (currentPath != null && cPaths <= this.nPaths);
+        List<E> currentPath;
+        this.pathList = new ArrayList<>();
+        BellmanFordShortestPath<V, E> bellmanFordShortestPath;
+        int cPaths = 1;
+        do {
+            setUp(cPaths);
+            bellmanFordShortestPath = new BellmanFordShortestPath<V, E>(this.graph, this.startVertex);
+            currentPath = bellmanFordShortestPath.getPathEdgeList(endVertex);
+            if (currentPath != null) {
+                cPaths++;                
+                pathList.add(currentPath);
+            }            
+        } while (currentPath != null && cPaths <= this.nPaths);
 
-		return pathList.size() > 0 ? resolvePaths(endVertex) : null;
-	}
+        return pathList.size() > 0 ? resolvePaths(endVertex) : null;
+    }
 
-	/**
-	 * Prepares the graph for a search of the next path:
-	 * Replacing the edges of the previous path with reversed edges
-	 * with negative weight
-	 * 
-	 * @param cPath the number of the next path to search 
-	 */
-	private void setUp(int cPath) {
-		//no setup for first path
-		if (cPath == 1) {
-			return;
-		}
-		
-		V source, target;
-		E reversedEdge;
-		//replace previous path edges with reversed edges with negative weight
-		for (E originalEdge : this.pathList.get(cPath - 2)) {
-			source = graph.getEdgeSource(originalEdge);
-			target = graph.getEdgeTarget(originalEdge);
-			graph.removeEdge(originalEdge);	
-			reversedEdge = graph.addEdge(target, source);
-			((WeightedGraph<V, E>) graph).setEdgeWeight(reversedEdge, - graph.getEdgeWeight(originalEdge));
-		}
-	}
-	
-	/**
-	 * At the end of the search we have list of intermediate paths - not necessarily
-	 * disjoint and may contain reversed edges. Here we go over all, removing overlapping
-	 * edges and merging them to valid paths (from start to end). Finally, we sort
-	 * them according to their weight.
-	 * 
-	 * @param endVertex the end vertex
-	 * 
-	 * @return sorted list of disjoint paths from start vertex to end vertex.
-	 */
-	private List<GraphPath<V, E>> resolvePaths(V endVertex) {
-		//first we need to remove overlapping edges.		
-		removeOverlappingEdges();
-		
-		//now we might be left with path fragments (not necessarily leading from start to end).
-		//We need to merge them to valid paths.
-		List<GraphPath<V, E>> paths = mergePaths(endVertex);
-		
-		//sort paths by overall weight (ascending)
-		Collections.sort(paths, (o1, o2) -> Double.compare(o1.getWeight(), o2.getWeight()));		
-		return paths;
-	}
-	
-	/**
-	 * After removing overlapping edges, each path is not necessarily connecting
-	 * start to end vertex. Here we connect the path fragments to valid paths
-	 * (from start to end).
-	 * 
-	 * @param endVertex the end vertex
-	 * 
-	 * @return list of disjoint paths from start to end.
-	 */
-	private List<GraphPath<V, E>> mergePaths(V endVertex) {
-		List<GraphPath<V, E>> graphPaths = new ArrayList<GraphPath<V, E>>();
-		for (int i = 0; i < this.pathList.size(); i++) {			
-			V nextHop = this.startVertex;
-			List<E> mergedPath = new ArrayList<E>();
-			while (! nextHop.equals(endVertex)) {
-				E nextEdge = null;
-				//search an edge connecting current and next hops at any of the paths
-				//once it is found, add it to the current (merged) path and remove it
-				//from the original path so it will not be used anymore.
-				for (List<E> path : pathList) {
-					boolean connectionFound = false;
-					Iterator<E> pathIter = path.iterator();
-					while (pathIter.hasNext()) {
-						E edge = pathIter.next();
-						if (graph.getEdgeSource(edge).equals(nextHop) || graph.getEdgeTarget(edge).equals(nextHop)) {
-							nextEdge = edge;
-							//remove edge so it will not be used again in any other path.
-							pathIter.remove();
-							connectionFound = true;
-							break;
-						}
-					}
-					if (connectionFound) {
-						break;
-					}
-				}
-				if (nextEdge == null) {
-					throw new IllegalArgumentException("Could not find a path from start to end vertex");
-				}
-				mergedPath.add(nextEdge);
-				nextHop = graph.getEdgeSource(nextEdge).equals(nextHop) ? 
-						graph.getEdgeTarget(nextEdge) : graph.getEdgeSource(nextEdge);
-			}
-			//path is ready, wrap it up.
-			graphPaths.add(createGraphPath(mergedPath, endVertex));
-		}
-		return graphPaths;
-	}
-	
-	/**
-	 * Iterating over all paths to removes overlapping edges (contained
-	 * in more than single path). At the end of this method, each path
-	 * contains unique edges but not necessarily connecting the start
-	 * to end vertex.
-	 * 
-	 */
-	private void removeOverlappingEdges() {
-		Iterator<E> path1Iter, path2Iter;
-		E e1, e2;
-		boolean found;
-		//removing overlapping edges
-		for (List<E> path1 : pathList) {
-			path1Iter = path1.iterator();
-			while (path1Iter.hasNext()) {
-				e1 = path1Iter.next();
-				found = false;
-				for (List<E> path2 : pathList) {
-					if (path2 == path1) {
-						continue;
-					}
-					path2Iter = path2.iterator();
-					while (path2Iter.hasNext()) {
-						e2 = path2Iter.next();
-						//graph is directed, checking both options.
-						if ((graph.getEdgeSource(e1).equals(graph.getEdgeSource(e2)) &&
-								graph.getEdgeTarget(e1).equals(graph.getEdgeTarget(e2))) ||
-								
-								(graph.getEdgeSource(e1).equals(graph.getEdgeTarget(e2)) &&
-										graph.getEdgeTarget(e1).equals(graph.getEdgeSource(e2)))) {
-							found = true;
-							path2Iter.remove();
-						}
-					}
-				}
-				if (found) {
-					path1Iter.remove();
-				}
-			}
-		}
-		
-	}
+    /**
+     * Prepares the graph for a search of the next path:
+     * Replacing the edges of the previous path with reversed edges
+     * with negative weight
+     * 
+     * @param cPath the number of the next path to search 
+     */
+    private void setUp(int cPath) {
+        //no setup for first path
+        if (cPath == 1) {
+            return;
+        }
+        
+        V source, target;
+        E reversedEdge;
+        //replace previous path edges with reversed edges with negative weight
+        for (E originalEdge : this.pathList.get(cPath - 2)) {
+            source = graph.getEdgeSource(originalEdge);
+            target = graph.getEdgeTarget(originalEdge);
+            graph.removeEdge(originalEdge);    
+            reversedEdge = graph.addEdge(target, source);
+            ((WeightedGraph<V, E>) graph).setEdgeWeight(reversedEdge, - graph.getEdgeWeight(originalEdge));
+        }
+    }
+    
+    /**
+     * At the end of the search we have list of intermediate paths - not necessarily
+     * disjoint and may contain reversed edges. Here we go over all, removing overlapping
+     * edges and merging them to valid paths (from start to end). Finally, we sort
+     * them according to their weight.
+     * 
+     * @param endVertex the end vertex
+     * 
+     * @return sorted list of disjoint paths from start vertex to end vertex.
+     */
+    private List<GraphPath<V, E>> resolvePaths(V endVertex) {
+        //first we need to remove overlapping edges.        
+        removeOverlappingEdges();
+        
+        //now we might be left with path fragments (not necessarily leading from start to end).
+        //We need to merge them to valid paths.
+        List<GraphPath<V, E>> paths = mergePaths(endVertex);
+        
+        //sort paths by overall weight (ascending)
+        Collections.sort(paths, (o1, o2) -> Double.compare(o1.getWeight(), o2.getWeight()));        
+        return paths;
+    }
+    
+    /**
+     * After removing overlapping edges, each path is not necessarily connecting
+     * start to end vertex. Here we connect the path fragments to valid paths
+     * (from start to end).
+     * 
+     * @param endVertex the end vertex
+     * 
+     * @return list of disjoint paths from start to end.
+     */
+    private List<GraphPath<V, E>> mergePaths(V endVertex) {
+        List<GraphPath<V, E>> graphPaths = new ArrayList<GraphPath<V, E>>();
+        for (int i = 0; i < this.pathList.size(); i++) {            
+            V nextHop = this.startVertex;
+            List<E> mergedPath = new ArrayList<E>();
+            while (! nextHop.equals(endVertex)) {
+                E nextEdge = null;
+                //search an edge connecting current and next hops at any of the paths
+                //once it is found, add it to the current (merged) path and remove it
+                //from the original path so it will not be used anymore.
+                for (List<E> path : pathList) {
+                    boolean connectionFound = false;
+                    Iterator<E> pathIter = path.iterator();
+                    while (pathIter.hasNext()) {
+                        E edge = pathIter.next();
+                        if (graph.getEdgeSource(edge).equals(nextHop) || graph.getEdgeTarget(edge).equals(nextHop)) {
+                            nextEdge = edge;
+                            //remove edge so it will not be used again in any other path.
+                            pathIter.remove();
+                            connectionFound = true;
+                            break;
+                        }
+                    }
+                    if (connectionFound) {
+                        break;
+                    }
+                }
+                if (nextEdge == null) {
+                    throw new IllegalArgumentException("Could not find a path from start to end vertex");
+                }
+                mergedPath.add(nextEdge);
+                nextHop = graph.getEdgeSource(nextEdge).equals(nextHop) ? 
+                        graph.getEdgeTarget(nextEdge) : graph.getEdgeSource(nextEdge);
+            }
+            //path is ready, wrap it up.
+            graphPaths.add(createGraphPath(mergedPath, endVertex));
+        }
+        return graphPaths;
+    }
+    
+    /**
+     * Iterating over all paths to removes overlapping edges (contained
+     * in more than single path). At the end of this method, each path
+     * contains unique edges but not necessarily connecting the start
+     * to end vertex.
+     * 
+     */
+    private void removeOverlappingEdges() {
+        Iterator<E> path1Iter, path2Iter;
+        E e1, e2;
+        boolean found;
+        //removing overlapping edges
+        for (List<E> path1 : pathList) {
+            path1Iter = path1.iterator();
+            while (path1Iter.hasNext()) {
+                e1 = path1Iter.next();
+                found = false;
+                for (List<E> path2 : pathList) {
+                    if (path2 == path1) {
+                        continue;
+                    }
+                    path2Iter = path2.iterator();
+                    while (path2Iter.hasNext()) {
+                        e2 = path2Iter.next();
+                        //graph is directed, checking both options.
+                        if ((graph.getEdgeSource(e1).equals(graph.getEdgeSource(e2)) &&
+                                graph.getEdgeTarget(e1).equals(graph.getEdgeTarget(e2))) ||
+                                
+                                (graph.getEdgeSource(e1).equals(graph.getEdgeTarget(e2)) &&
+                                        graph.getEdgeTarget(e1).equals(graph.getEdgeSource(e2)))) {
+                            found = true;
+                            path2Iter.remove();
+                        }
+                    }
+                }
+                if (found) {
+                    path1Iter.remove();
+                }
+            }
+        }
+        
+    }
 
-	private void assertGetPaths(V endVertex) {
-		if (endVertex == null) {
-			throw new NullPointerException("endVertex is null");
-		}
-		if (endVertex.equals(this.startVertex)) {
-			throw new IllegalArgumentException("The end vertex is the same as the start vertex!");
-		}
-		if (!this.graph.vertexSet().contains(endVertex)) {
-			throw new IllegalArgumentException("Graph must contain the end vertex!");
-		}
-	}
+    private void assertGetPaths(V endVertex) {
+        if (endVertex == null) {
+            throw new NullPointerException("endVertex is null");
+        }
+        if (endVertex.equals(this.startVertex)) {
+            throw new IllegalArgumentException("The end vertex is the same as the start vertex!");
+        }
+        if (!this.graph.vertexSet().contains(endVertex)) {
+            throw new IllegalArgumentException("Graph must contain the end vertex!");
+        }
+    }
 
-	private void assertKShortestPathsFinder(Graph<V, E> graph, V startVertex, int nPaths) {
-		if (graph == null) {
-			throw new NullPointerException("graph is null");
-		}
-		if (startVertex == null) {
-			throw new NullPointerException("startVertex is null");
-		}
-		if (nPaths <= 0) {
-			throw new NullPointerException("nPaths is negative or 0");
-		}
-	}
-	
-	private GraphPath<V, E> createGraphPath(List<E> edgeList, V endVertex) {
-		double weight = 0;
-		for (E edge : edgeList) {
-			weight += graph.getEdgeWeight(edge);
-		}
-		return new GraphWalk<>(graph, startVertex, endVertex, edgeList, weight);
-	}
+    private void assertKShortestPathsFinder(Graph<V, E> graph, V startVertex, int nPaths) {
+        if (graph == null) {
+            throw new NullPointerException("graph is null");
+        }
+        if (startVertex == null) {
+            throw new NullPointerException("startVertex is null");
+        }
+        if (nPaths <= 0) {
+            throw new NullPointerException("nPaths is negative or 0");
+        }
+    }
+    
+    private GraphPath<V, E> createGraphPath(List<E> edgeList, V endVertex) {
+        double weight = 0;
+        for (E edge : edgeList) {
+            weight += graph.getEdgeWeight(edge);
+        }
+        return new GraphWalk<>(graph, startVertex, endVertex, edgeList, weight);
+    }
 }
 
 // End .java
