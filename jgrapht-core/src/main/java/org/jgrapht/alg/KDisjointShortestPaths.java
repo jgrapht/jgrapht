95c36cf730a5bafc820f8e1b2b0e88821873b877
mizrachi <assaf.mizrachi@ecitele.com>
2016-11-02 09:58:01 +0200

    Using standard header format.

--- jgrapht-core/src/main/java/org/jgrapht/alg/KDisjointShortestPaths.java
+++ jgrapht-core/src/main/java/org/jgrapht/alg/KDisjointShortestPaths.java
@@ -1,11 +1,7 @@
-/* ==========================================
+/*
+ * (C) Copyright 2016-2016, by Assaf Mizrachi and Contributors.
+ *
  * JGraphT : a free Java graph-theory library
- * ==========================================
- *
- * Project Info:  http://jgrapht.sourceforge.net/
- * Project Creator:  Barak Naveh (http://sourceforge.net/users/barak_naveh)
- *
- * (C) Copyright 2003-2016, by Barak Naveh and Contributors.
  *
  * This program and the accompanying materials are dual-licensed under
  * either
@@ -18,21 +14,6 @@
  *
  * (b) the terms of the Eclipse Public License v1.0 as published by
  * the Eclipse Foundation.
- */
-/* -------------------------
- * KDisjointShortestPaths.java
- * -------------------------
- * (C) Copyright 2016, by Assaf Mizrachi and Contributors.
- *
- * Original Author: Assaf Mizrachi.
- * Contributor(s):
- *
- * $Id$
- *
- * Changes
- * -------
- * 11-Sep-2016 : Initial revision (AM);
- *
  */
 package org.jgrapht.alg;
 
