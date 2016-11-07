f26fc1ea8a22438dc943411c9b15b45a2435f114
Bazco Industries <bazco@runbox.com>
2016-11-06 22:34:54 -0500

    Tabs to spaces in StringUtility.java

--- jgrapht-ext/src/main/java/org/jgrapht/ext/StringUtility.java
+++ jgrapht-ext/src/main/java/org/jgrapht/ext/StringUtility.java
@@ -2,25 +2,25 @@ package org.jgrapht.ext;
 
 public class StringUtility
 {
-	/**
-	 * Removes quotes from a string
-	 * 
-	 * @param in a quoted string
-	 * @return the string without enclosing quotes; if <code>in</code> is not
-	 *         quoted, the original string is returned.
-	 */
-	public static String deQuote (String in)
-	{
-		int len = in.length();
-		
-		if (in.charAt(0) == '\"' && in.charAt(len - 1) == '\"') {
-			if (len > 2) {
-				return in.substring(1, len - 1);
-			} else {
-				return "";
-			}
-		}
+    /**
+     * Removes quotes from a string
+     * 
+     * @param in a quoted string
+     * @return the string without enclosing quotes; if <code>in</code> is not
+     *         quoted, the original string is returned.
+     */
+    public static String deQuote (String in)
+    {
+        int len = in.length();
 
-		return in;
-	}
+        if (in.charAt(0) == '\"' && in.charAt(len - 1) == '\"') {
+            if (len > 2) {
+                return in.substring(1, len - 1);
+            } else {
+                return "";
+            }
+        }
+
+        return in;
+    }
 }
