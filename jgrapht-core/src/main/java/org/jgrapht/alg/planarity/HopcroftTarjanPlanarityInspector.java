57b431595afbe25f741bf712961d0833fa16344c
karolina_rezkova <karolina.rezkova@gmail.com>
2017-06-20 12:31:36 +0200

    Update HopcroftTarjanPlanarityInspector.java

--- jgrapht-core/src/main/java/org/jgrapht/alg/planarity/HopcroftTarjanPlanarityInspector.java
+++ jgrapht-core/src/main/java/org/jgrapht/alg/planarity/HopcroftTarjanPlanarityInspector.java
@@ -1,12 +1,19 @@
 /*
  * (C) Copyright 2017-2017, by Karolina Rezkova and Contributors.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- *    Karolina - initial API and implementation and/or initial documentation
+ * JGraphT : a free Java graph-theory library
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either
+ *
+ * (a) the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation, or (at your option) any
+ * later version.
+ *
+ * or (per the licensee's choosing)
+ *
+ * (b) the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation.
  */
 package org.jgrapht.alg.planarity;
 
