017c1c67660398f54c5fd56c2b92bd0bbe3691bb
Meghana M Reddy <36499567+meghanamreddy@users.noreply.github.com>
2018-02-25 15:47:30 +0530

    Fixed some indentation errors

--- jgrapht-core/src/test/java/org/jgrapht/alg/color/BipartiteColoringTest.java
+++ jgrapht-core/src/test/java/org/jgrapht/alg/color/BipartiteColoringTest.java
@@ -30,38 +30,38 @@ import org.jgrapht.graph.Pseudograph;
  *
  */
 public class BipartiteColoringTest {
-	public void test1() {
-		Graph<String, DefaultEdge> dg = new Pseudograph<>(DefaultEdge.class);
+    public void test1() {
+        Graph<String, DefaultEdge> dg = new Pseudograph<>(DefaultEdge.class);
+
+        String a = new String("a");
+        String b = new String("b");
+        String c = new String("c");
+        String d = new String("d");
+        String e = new String("e");
+        String f = new String("f");
+        String g = new String("g");
+
+        dg.addVertex(a);
+        dg.addVertex(b);
+        dg.addVertex(c);
+        dg.addVertex(d);
+        dg.addVertex(e);
+        dg.addVertex(f);
+        dg.addVertex(g);
+        dg.addEdge(a, d);
+        dg.addEdge(a, e);
+        dg.addEdge(g, a);
+        dg.addEdge(c, e);
+        dg.addEdge(c, f);
+        dg.addEdge(b, g);
+
+        BipartiteColoring<String, DefaultEdge> bipartiteColorer = new BipartiteColoring<>(dg);
+        Map<String, Integer> ccs = bipartiteColorer.findTwoColoring(dg);
+        System.out.println(ccs);
+    }
 	
-		String a = new String("a");
-		String b = new String("b");
-		String c = new String("c");
-		String d = new String("d");
-		String e = new String("e");
-		String f = new String("f");
-		String g = new String("g");
-		
-		dg.addVertex(a);
-		dg.addVertex(b);
-		dg.addVertex(c);
-		dg.addVertex(d);
-		dg.addVertex(e);
-		dg.addVertex(f);
-		dg.addVertex(g);
-		dg.addEdge(a, d);
-		dg.addEdge(a, e);
-		dg.addEdge(g, a);
-		dg.addEdge(c, e);
-		dg.addEdge(c, f);
-		dg.addEdge(b, g);
-		
-		BipartiteColoring<String, DefaultEdge> bipartiteColorer = new BipartiteColoring<>(dg);
-		Map<String, Integer> ccs = bipartiteColorer.findTwoColoring(dg);
-		System.out.println(ccs);
-	}
-	
-	public static void main(String[] args) {
-		BipartiteColoringTest tester = new BipartiteColoringTest();
-		tester.test1();
-	}
+    public static void main(String[] args) {
+        BipartiteColoringTest tester = new BipartiteColoringTest();
+        tester.test1();
+    }
 }
