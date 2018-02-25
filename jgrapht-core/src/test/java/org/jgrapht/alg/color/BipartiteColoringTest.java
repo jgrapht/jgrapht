2b77c09c4e9035c960ce51987879f7e51873acae
Meghana M Reddy <36499567+meghanamreddy@users.noreply.github.com>
2018-02-25 13:50:40 +0530

    Update BipartiteColoringTest.java

--- jgrapht-core/src/test/java/org/jgrapht/alg/color/BipartiteColoringTest.java
+++ jgrapht-core/src/test/java/org/jgrapht/alg/color/BipartiteColoringTest.java
@@ -1,4 +1,21 @@
-package jgraph_v1;
+/*
+ * (C) Copyright 2017-2017, by Dimitrios Michail and Contributors.
+ *
+ * JGraphT : a free Java graph-theory library
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either
+ *
+ * (a) the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation, or (at your option) any
+ * later version.
+ *
+ * or (per the licensee's choosing)
+ *
+ * (b) the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation.
+ */
+package org.jgrapht.alg.color;
 
 import java.util.Map;
 
