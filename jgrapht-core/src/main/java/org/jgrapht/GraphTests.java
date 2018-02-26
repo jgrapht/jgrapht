117e7a6ac4a900f286cae26a8fb853ab46c2b110
Alexandru Văleanu <valeanu_alex@yahoo.com>
2018-02-26 10:45:38 +0000

    Replace ≥ with \geq

--- jgrapht-core/src/main/java/org/jgrapht/GraphTests.java
+++ jgrapht-core/src/main/java/org/jgrapht/GraphTests.java
@@ -482,9 +482,9 @@ public abstract class GraphTests
     /**
      * Tests whether an undirected graph meets Ore's condition to be Hamiltonian.
      *
-     * Let G be a (finite and simple) graph with $n ≥ 3$ vertices. We denote by deg(v) the degree of a vertex v in G,
+     * Let G be a (finite and simple) graph with $n \geq 3$ vertices. We denote by deg(v) the degree of a vertex v in G,
      * i.e. the number of incident edges in G to v.
-     * Then, Ore's theorem states that if $deg(v) + deg(w) ≥ n$ for every pair of distinct non-adjacent vertices
+     * Then, Ore's theorem states that if $deg(v) + deg(w) \geq n$ for every pair of distinct non-adjacent vertices
      * v and w of G, then G is Hamiltonian.
      *
      * @param graph the input graph
