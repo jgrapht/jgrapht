a7970701a367a4466cf05981106fc9aabe5b5e24
Peter Harman <peter.harman@deltatheta.com>
2018-06-13 12:31:09 +0100

    Improved DulmageMendelsohnDecompositionTest by asserting correct structure of decomposition and running for a range of graph sizes, with random imbalance between size of partition 1 and 2.

--- jgrapht-core/src/test/java/org/jgrapht/alg/connectivity/DulmageMendelsohnDecompositionTest.java
+++ jgrapht-core/src/test/java/org/jgrapht/alg/connectivity/DulmageMendelsohnDecompositionTest.java
@@ -17,22 +17,19 @@
  */
 package org.jgrapht.alg.connectivity;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Random;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import org.jgrapht.Graph;
-import org.jgrapht.Graphs;
+import org.jgrapht.alg.interfaces.MatchingAlgorithm.Matching;
+import org.jgrapht.alg.matching.HopcroftKarpMaximumCardinalityBipartiteMatching;
 import org.jgrapht.generate.GnmRandomBipartiteGraphGenerator;
+import org.jgrapht.graph.AsSubgraph;
 import org.jgrapht.graph.DefaultEdge;
-import org.jgrapht.graph.Pseudograph;
 import org.jgrapht.graph.SimpleGraph;
 import org.jgrapht.util.SupplierUtil;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import org.junit.Test;
 
 /**
@@ -44,174 +41,110 @@ import org.junit.Test;
  */
 public class DulmageMendelsohnDecompositionTest {
 
-    /**
-     * Random test graph 1
-     */
-    @Test
-    public void testBipartiteMatching1() {
-        Graph<Integer, DefaultEdge> graph = new SimpleGraph<>(DefaultEdge.class);
-        List<Integer> partition1 = Arrays.asList(0, 1, 2, 3);
-        List<Integer> partition2 = Arrays.asList(4, 5, 6, 7);
-        Graphs.addAllVertices(graph, partition1);
-        Graphs.addAllVertices(graph, partition2);
-
-        DefaultEdge e02 = graph.addEdge(partition1.get(0), partition2.get(2));
-        DefaultEdge e11 = graph.addEdge(partition1.get(1), partition2.get(1));
-        DefaultEdge e20 = graph.addEdge(partition1.get(2), partition2.get(0));
-
-        DulmageMendelsohnDecomposition<Integer, DefaultEdge> dm = new DulmageMendelsohnDecomposition<>(
-                graph, new HashSet<>(partition1), new HashSet<>(partition2));
-        DulmageMendelsohnDecomposition.Decomposition<Integer, DefaultEdge> course = dm.getDecomposition(false);
-        assertTrue(course.getPartition1DominatedSet().size() + course.getPartition2DominatedSet().size() + course.getPerfectMatchedSets().get(0).size() == graph.vertexSet().size());
-        DulmageMendelsohnDecomposition.Decomposition<Integer, DefaultEdge> fine = dm.getDecomposition(true);
-        assertTrue(fine.getPartition1DominatedSet().size() + fine.getPartition2DominatedSet().size() + count(fine.getPerfectMatchedSets()) == graph.vertexSet().size());
-
-    }
-
-    /**
-     * Random test graph 2
-     */
-    @Test
-    public void testBipartiteMatching2() {
-        Graph<Integer, DefaultEdge> graph = new SimpleGraph<>(DefaultEdge.class);
-        List<Integer> partition1 = Arrays.asList(0, 1, 2, 3, 4, 5);
-        List<Integer> partition2 = Arrays.asList(6, 7, 8, 9, 10, 11);
-        Graphs.addAllVertices(graph, partition1);
-        Graphs.addAllVertices(graph, partition2);
-
-        DefaultEdge e00 = graph.addEdge(partition1.get(0), partition2.get(0));
-        DefaultEdge e13 = graph.addEdge(partition1.get(1), partition2.get(3));
-        DefaultEdge e21 = graph.addEdge(partition1.get(2), partition2.get(1));
-        DefaultEdge e34 = graph.addEdge(partition1.get(3), partition2.get(4));
-        DefaultEdge e42 = graph.addEdge(partition1.get(4), partition2.get(2));
-        DefaultEdge e55 = graph.addEdge(partition1.get(5), partition2.get(5));
-
-        DulmageMendelsohnDecomposition<Integer, DefaultEdge> dm = new DulmageMendelsohnDecomposition<>(
-                graph, new HashSet<>(partition1), new HashSet<>(partition2));
-        DulmageMendelsohnDecomposition.Decomposition<Integer, DefaultEdge> course = dm.getDecomposition(false);
-        assertTrue(course.getPartition1DominatedSet().size() + course.getPartition2DominatedSet().size() + course.getPerfectMatchedSets().get(0).size() == graph.vertexSet().size());
-        DulmageMendelsohnDecomposition.Decomposition<Integer, DefaultEdge> fine = dm.getDecomposition(true);
-        assertTrue(fine.getPartition1DominatedSet().size() + fine.getPartition2DominatedSet().size() + count(fine.getPerfectMatchedSets()) == graph.vertexSet().size());
-
-    }
-
-    /**
-     * Find a maximum matching on a graph without edges
-     */
-    @Test
-    public void testEmptyMatching() {
-        Graph<Integer, DefaultEdge> graph = new SimpleGraph<>(DefaultEdge.class);
-        List<Integer> partition1 = Collections.singletonList(0);
-        List<Integer> partition2 = Collections.singletonList(1);
-        Graphs.addAllVertices(graph, partition1);
-        Graphs.addAllVertices(graph, partition2);
-        DulmageMendelsohnDecomposition<Integer, DefaultEdge> dm = new DulmageMendelsohnDecomposition<>(
-                graph, new HashSet<>(partition1), new HashSet<>(partition2));
-        DulmageMendelsohnDecomposition.Decomposition<Integer, DefaultEdge> course = dm.getDecomposition(false);
-        assertTrue(course.getPartition1DominatedSet().size() + course.getPartition2DominatedSet().size() + course.getPerfectMatchedSets().get(0).size() == graph.vertexSet().size());
-        DulmageMendelsohnDecomposition.Decomposition<Integer, DefaultEdge> fine = dm.getDecomposition(true);
-        assertTrue(fine.getPartition1DominatedSet().size() + fine.getPartition2DominatedSet().size() + count(fine.getPerfectMatchedSets()) == graph.vertexSet().size());
-
-    }
-
-    @Test
-    public void testGraph1() {
-        Graph<Integer, DefaultEdge> graph = new SimpleGraph<>(DefaultEdge.class);
-        Set<Integer> partition1 = new HashSet<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6));
-        Set<Integer> partition2 = new HashSet<>(Arrays.asList(7, 8, 9));
-        Graphs.addAllVertices(graph, partition1);
-        Graphs.addAllVertices(graph, partition2);
-        int[][] edges = {{5, 8}, {4, 9}, {2, 7}, {6, 9}, {1, 9}};
-        for (int[] edge : edges) {
-            graph.addEdge(edge[0], edge[1]);
-        }
-
-        DulmageMendelsohnDecomposition<Integer, DefaultEdge> dm = new DulmageMendelsohnDecomposition<>(
-                graph, partition1, partition2);
-        DulmageMendelsohnDecomposition.Decomposition<Integer, DefaultEdge> course = dm.getDecomposition(false);
-        assertTrue(course.getPartition1DominatedSet().size() + course.getPartition2DominatedSet().size() + course.getPerfectMatchedSets().get(0).size() == graph.vertexSet().size());
-        DulmageMendelsohnDecomposition.Decomposition<Integer, DefaultEdge> fine = dm.getDecomposition(true);
-        assertTrue(fine.getPartition1DominatedSet().size() + fine.getPartition2DominatedSet().size() + count(fine.getPerfectMatchedSets()) == graph.vertexSet().size());
-
-    }
-
-    @Test
-    public void testGraph2() {
-        Graph<Integer, DefaultEdge> graph = new SimpleGraph<>(DefaultEdge.class);
-        Set<Integer> partition1 = new HashSet<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6));
-        Set<Integer> partition2 = new HashSet<>(Arrays.asList(7, 8, 9));
-        Graphs.addAllVertices(graph, partition1);
-        Graphs.addAllVertices(graph, partition2);
-        int[][] edges
-                = {{5, 8}, {4, 9}, {2, 7}, {6, 9}, {1, 9}, {0, 8}, {3, 7}, {1, 7}};
-        for (int[] edge : edges) {
-            graph.addEdge(edge[0], edge[1]);
-        }
-
-        DulmageMendelsohnDecomposition<Integer, DefaultEdge> dm = new DulmageMendelsohnDecomposition<>(
-                graph, partition1, partition2);
-        DulmageMendelsohnDecomposition.Decomposition<Integer, DefaultEdge> course = dm.getDecomposition(false);
-        assertTrue(course.getPartition1DominatedSet().size() + course.getPartition2DominatedSet().size() + course.getPerfectMatchedSets().get(0).size() == graph.vertexSet().size());
-        DulmageMendelsohnDecomposition.Decomposition<Integer, DefaultEdge> fine = dm.getDecomposition(true);
-        assertTrue(fine.getPartition1DominatedSet().size() + fine.getPartition2DominatedSet().size() + count(fine.getPerfectMatchedSets()) == graph.vertexSet().size());
-
-    }
-
-    /**
-     * Issue 233 instance
-     */
-    @Test
-    public void testBipartiteMatchingIssue233() {
-        Graph<Integer, DefaultEdge> g = new SimpleGraph<>(DefaultEdge.class);
-
-        Graphs.addAllVertices(g, IntStream.rangeClosed(0, 3).boxed().collect(Collectors.toList()));
-
-        Set<Integer> left = new HashSet<>(Arrays.asList(0, 1));
-        Set<Integer> right = new HashSet<>(Arrays.asList(2, 3));
-
-        g.addEdge(0, 2);
-        g.addEdge(0, 3);
-        g.addEdge(1, 2);
-
-        DulmageMendelsohnDecomposition<Integer, DefaultEdge> dm = new DulmageMendelsohnDecomposition<>(
-                g, left, right);
-        DulmageMendelsohnDecomposition.Decomposition<Integer, DefaultEdge> course = dm.getDecomposition(false);
-        assertTrue(course.getPartition1DominatedSet().size() + course.getPartition2DominatedSet().size() + course.getPerfectMatchedSets().get(0).size() == g.vertexSet().size());
-        DulmageMendelsohnDecomposition.Decomposition<Integer, DefaultEdge> fine = dm.getDecomposition(true);
-        assertTrue(fine.getPartition1DominatedSet().size() + fine.getPartition2DominatedSet().size() + count(fine.getPerfectMatchedSets()) == g.vertexSet().size());
-    }
-
     @Test
     public void testRandomBipartiteGraphs() {
         Random random = new Random(1);
-        int vertices = 100;
-
-        for (int k = 0; k < 100; k++) {
+        for (int vertices = 20; vertices < 200; vertices++) {
             int edges = random.nextInt(maxEdges(vertices) / 2);
+            int imbalance = randomImbalance(random, vertices);
             GnmRandomBipartiteGraphGenerator<Integer, DefaultEdge> generator
-                    = new GnmRandomBipartiteGraphGenerator<>(vertices, vertices, edges, 0);
+                    = new GnmRandomBipartiteGraphGenerator<>(vertices - imbalance, vertices + imbalance, edges, 0);
             Graph<Integer, DefaultEdge> graph = new SimpleGraph<>(SupplierUtil.createIntegerSupplier(), SupplierUtil.DEFAULT_EDGE_SUPPLIER, false);
             generator.generateGraph(graph);
             DulmageMendelsohnDecomposition<Integer, DefaultEdge> dm = new DulmageMendelsohnDecomposition<>(
                     graph, generator.getFirstPartition(), generator.getSecondPartition());
-            DulmageMendelsohnDecomposition.Decomposition<Integer, DefaultEdge> course = dm.getDecomposition(false);
-            assertTrue(course.getPartition1DominatedSet().size() + course.getPartition2DominatedSet().size() + course.getPerfectMatchedSets().get(0).size() == vertices * 2);
-            DulmageMendelsohnDecomposition.Decomposition<Integer, DefaultEdge> fine = dm.getDecomposition(true);
-            assertTrue(fine.getPartition1DominatedSet().size() + fine.getPartition2DominatedSet().size() + count(fine.getPerfectMatchedSets()) == vertices * 2);
+            assertValidDecomposition(graph, dm.getDecomposition(true), generator.getFirstPartition(), generator.getSecondPartition());
+            assertValidDecomposition(graph, dm.getDecomposition(false), generator.getFirstPartition(), generator.getSecondPartition());
         }
     }
 
-    private static <V> int count(List<Set<V>> sets) {
-        int n = 0;
-        n = sets.stream().map((set) -> set.size()).reduce(n, Integer::sum);
-        return n;
+    /**
+     * Assert that the structure of the decomposition is valid
+     *
+     * @param <V>
+     * @param <E>
+     * @param graph
+     * @param decomposition
+     * @param partition1
+     * @param partition2
+     */
+    private static <V, E> void assertValidDecomposition(Graph<V, E> graph, DulmageMendelsohnDecomposition.Decomposition<V, E> decomposition, Set<V> partition1, Set<V> partition2) {
+        // Is the perfect matched set actually perfectly matched?
+        Set<V> allPerfectlyMatched = new HashSet<>();
+        Set<V> partition1PerfectlyMatched = new HashSet<>();
+        Set<V> partition2PerfectlyMatched = new HashSet<>();
+        for (Set<V> set : decomposition.getPerfectMatchedSets()) {
+            allPerfectlyMatched.addAll(set);
+            for (V v : set) {
+                if (partition1.contains(v)) {
+                    partition1PerfectlyMatched.add(v);
+                }
+                if (partition2.contains(v)) {
+                    partition2PerfectlyMatched.add(v);
+                }
+            };
+        };
+        Matching<V, E> perfectMatching = new HopcroftKarpMaximumCardinalityBipartiteMatching<>(new AsSubgraph<>(graph, allPerfectlyMatched), partition1PerfectlyMatched, partition2PerfectlyMatched).getMatching();
+        assertTrue("Core of decomposition must perfectly match", perfectMatching.isPerfect());
+        // Do all the vertices in the graph appear in the decomposition, and only in one part of it?
+        for (V v : graph.vertexSet()) {
+            if (allPerfectlyMatched.contains(v)) {
+                assertFalse("Vertex appears in multiple sets in decomposition", decomposition.getPartition1DominatedSet().contains(v));
+                assertFalse("Vertex appears in multiple sets in decomposition", decomposition.getPartition2DominatedSet().contains(v));
+            } else if (decomposition.getPartition1DominatedSet().contains(v)) {
+                assertFalse("Vertex appears in multiple sets in decomposition", allPerfectlyMatched.contains(v));
+                assertFalse("Vertex appears in multiple sets in decomposition", decomposition.getPartition2DominatedSet().contains(v));
+            } else {
+                assertTrue("Vertex appears in multiple sets in decomposition", decomposition.getPartition2DominatedSet().contains(v));
+            }
+        };
+        // Are the partition1/2 dominated sets dominated as expected?
+        int n1 = 0;
+        int n2 = 0;
+        for (V v : decomposition.getPartition1DominatedSet()) {
+            if (partition1.contains(v)) {
+                n1++;
+            } else {
+                n2++;
+            }
+        }
+        assertTrue("Partition 1 dominated set is not dominated by partition 1", n1 > n2 || (n1 == 0 && n2 == 0));
+        n1 = 0;
+        n2 = 0;
+        for (V v : decomposition.getPartition2DominatedSet()) {
+            if (partition1.contains(v)) {
+                n1++;
+            } else {
+                n2++;
+            }
+        }
+        assertTrue("Partition 2 dominated set is not dominated by partition 2", n1 < n2 || (n1 == 0 && n2 == 0));
     }
 
+    /**
+     * Calculate the maximum number of edges for number of vertices
+     *
+     * @param n
+     * @return
+     */
     private static int maxEdges(int n) {
         if (n % 2 == 0) {
             return Math.multiplyExact(n / 2, n - 1);
         } else {
             return Math.multiplyExact(n, (n - 1) / 2);
         }
+    }
+
+    /**
+     * Generate a random difference between the size of partition1 and
+     * partition2
+     *
+     * @param random
+     * @param n
+     * @return
+     */
+    private static int randomImbalance(Random random, int n) {
+        int max = Math.floorDiv(n, 4);
+        return random.nextInt(max * 2) - max;
     }
 }
