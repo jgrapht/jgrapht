318c8019bd133967779d2fbf40d2d4ca6b5384bd
Alexandru Văleanu <valeanu_alex@yahoo.com>
2018-06-19 14:58:59 +0300

    Add Radix sort tests

--- jgrapht-core/src/main/java/org/jgrapht/util/RadixSort.java
+++ jgrapht-core/src/main/java/org/jgrapht/util/RadixSort.java
@@ -1,17 +1,43 @@
+/*
+ * (C) Copyright 2018-2018, by Alexandru Valeanu and Contributors.
+ *
+ * JGraphT : a free Java graph-theory library
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either
+ *
+ * (a) the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation, or (at your option) any
+ * later version.
+ *
+ * or (per the licensee's choosing)
+ *
+ * (b) the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation.
+ */
 package org.jgrapht.util;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.ListIterator;
 
+/**
+ * Sorts the specified list of integers into ascending order using the Radix Sort method.
+ *
+ * This algorithms runs in $O(N + V)$ time and uses $O(N + V)$ extra memory, where $V = 256$.
+ *
+ * The specified list must be modifiable, but need not be resizable.
+ */
 public class RadixSort {
 
-    public static final int MAX_DIGITS = 32;
-    public static final int MAX_D = 4;
-    public static final int SIZE_RADIX = 1 << (MAX_DIGITS / MAX_D);
-    public static final int MASK = SIZE_RADIX - 1;
+    private static final int MAX_DIGITS = 32;
+    private static final int MAX_D = 4;
+    private static final int SIZE_RADIX = 1 << (MAX_DIGITS / MAX_D);
+    private static final int MASK = SIZE_RADIX - 1;
 
     private static int[] C = new int[SIZE_RADIX];
 
+    // Suppresses default constructor, ensuring non-instantiability.
     private RadixSort(){
     }
 
@@ -32,19 +58,38 @@ public class RadixSort {
         }
     }
 
+    /**
+     * Sort the given list in ascending order.
+     *
+     * @param list the input list of integers
+     */
     public static void sort(List<Integer> list){
+        if (list == null){
+            return;
+        }
+
         final int n = list.size();
+
+        if (n <= 40){
+            list.sort(null);
+            return;
+        }
+
         int[] A = new int[n];
         int[] B = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            A[i] = list.get(i);
-        }
+        ListIterator<Integer> listIterator = list.listIterator();
 
+        while (listIterator.hasNext()){
+            A[listIterator.nextIndex()] = listIterator.next();
+        }
         radixSort(A, n, B, C);
 
-        for (int i = 0; i < n; i++) {
-            list.set(i, A[i]);
+        listIterator = list.listIterator();
+
+        while (listIterator.hasNext()){
+            listIterator.next();
+            listIterator.set(A[listIterator.previousIndex()]);
         }
     }
 }
