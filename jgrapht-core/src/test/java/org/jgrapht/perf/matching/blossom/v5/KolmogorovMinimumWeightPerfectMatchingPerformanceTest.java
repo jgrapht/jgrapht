6682763fe3076f0ba763a3b1c2f13611c7f7e5c8
Toptachamann <timofey.chudakov@gmail.com>
2018-08-13 22:40:25 +0300

    Final GSoC commit

--- jgrapht-core/src/test/java/org/jgrapht/perf/matching/blossom/v5/KolmogorovMinimumWeightPerfectMatchingPerformanceTest.java
+++ jgrapht-core/src/test/java/org/jgrapht/perf/matching/blossom/v5/KolmogorovMinimumWeightPerfectMatchingPerformanceTest.java
@@ -43,6 +43,7 @@ public class KolmogorovMinimumWeightPerfectMatchingPerformanceTest {
 
     @State(Scope.Benchmark)
     public static class Data {
+
         public BlossomVOptions[] options = BlossomVOptions.ALL_OPTIONS;
         @Param({"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"})
         public int optionNum;
