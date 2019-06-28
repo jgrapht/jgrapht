ec366ba8196e852d191761b47b72c28abe0ed675
bbk@blipsystems.com <bbk@blipsystems.com>
2019-06-28 10:06:36 +0200

    Added license info

--- jgrapht-core/src/main/java/org/jgrapht/alg/shortestpath/BhandariTreeGraftingKDisjointShortestPaths.java
+++ jgrapht-core/src/main/java/org/jgrapht/alg/shortestpath/BhandariTreeGraftingKDisjointShortestPaths.java
@@ -1,3 +1,21 @@
+/*
+ * (C) Copyright 2018-2018, by Benjamin Krogh and Contributors.
+ *
+ * JGraphT : a free Java graph-theory library
+ *
+ * See the CONTRIBUTORS.md file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the
+ * GNU Lesser General Public License v2.1 or later
+ * which is available at
+ * http://www.gnu.org/licenses/old-licenses/lgpl-2.1-standalone.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR LGPL-2.1-or-later
+ */
+
 package org.jgrapht.alg.shortestpath;
 
 import org.jgrapht.Graph;
@@ -33,7 +51,7 @@ import java.util.*;
  *
  * @param <V> the graph vertex type
  * @param <E> the graph edge type
- * @author Assaf Mizrachi
+ * @author Benjamin Krogh
  */
 public class BhandariTreeGraftingKDisjointShortestPaths<V, E>
         extends
