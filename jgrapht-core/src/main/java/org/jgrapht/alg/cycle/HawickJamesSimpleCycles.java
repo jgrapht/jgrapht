8bab55ca525b237e0ed79758a5fb0d5cdde49387
Edwin Ouwehand <e.ouwehand@sig.eu>
2019-09-24 12:20:25 +0200

    Add unit tests to HawickJamesSimpleCycles and extend its implementation with a maximum path limit

--- jgrapht-core/src/main/java/org/jgrapht/alg/cycle/HawickJamesSimpleCycles.java
+++ jgrapht-core/src/main/java/org/jgrapht/alg/cycle/HawickJamesSimpleCycles.java
@@ -17,9 +17,21 @@
  */
 package org.jgrapht.alg.cycle;
 
-import org.jgrapht.*;
+import org.jgrapht.Graph;
+import org.jgrapht.GraphTests;
+import org.jgrapht.Graphs;
 
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.IntStream;
+
+import static java.util.Collections.frequency;
+import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
 
 /**
  * Find all simple cycles of a directed graph using the algorithm described by Hawick and James.
@@ -34,46 +46,31 @@ import java.util.*;
  *
  * @author Luiz Kill
  */
-public class HawickJamesSimpleCycles<V, E>
-    implements
-    DirectedSimpleCycles<V, E>
-{
-    private enum Operation
-    {
-        ENUMERATE,
-        PRINT_ONLY,
-        COUNT_ONLY
-    }
+public class HawickJamesSimpleCycles<V, E> implements DirectedSimpleCycles<V, E> {
 
-    // The graph
     private Graph<V, E> graph;
-
-    // Number of vertices
-    private int nVertices = 0;
-
-    // Number of simple cycles
-    private long nCycles = 0;
-
-    // Simple cycles found
-    private List<List<V>> cycles = null;
+    private long nCycles;
+    private List<List<V>> cycles;
 
     // The main state of the algorithm
-    private Integer start = 0;
-    private List<Integer>[] Ak = null;
-    private List<Integer>[] B = null;
-    private boolean[] blocked = null;
-    private ArrayDeque<Integer> stack = null;
+    private Integer start;
+    private List<List<Integer>> adjacencyList;
+    private List<List<Integer>> blockedVertices;
+    private List<Boolean> blocked;
+    private ArrayDeque<Integer> stack;
 
-    // Giving an index to every V
-    private V[] iToV = null;
-    private Map<V, Integer> vToI = null;
+    // Indexing the vertices
+    private List<V> iToV;
+    private Map<V, Integer> vToI;
+
+    private Runnable operation;
+    private int pathLimit = 0;
+    private boolean hasLimit;
 
     /**
      * Create a simple cycle finder with an unspecified graph.
      */
-    public HawickJamesSimpleCycles()
-    {
-    }
+    public HawickJamesSimpleCycles() { }
 
     /**
      * Create a simple cycle finder for the specified graph.
@@ -83,264 +80,165 @@ public class HawickJamesSimpleCycles<V, E>
      * @throws IllegalArgumentException if the graph argument is <code>
      * null</code>.
      */
-    public HawickJamesSimpleCycles(Graph<V, E> graph)
-        throws IllegalArgumentException
-    {
+    public HawickJamesSimpleCycles(Graph<V, E> graph) throws IllegalArgumentException {
+        setGraph(graph);
+    }
+
+    /**
+     * Set the graph
+     *
+     * @param graph graph
+     */
+    public void setGraph(Graph<V, E> graph) {
         this.graph = GraphTests.requireDirected(graph, "Graph must be directed");
     }
 
-    @SuppressWarnings("unchecked")
-    private void initState(Operation o)
-    {
-        nCycles = 0;
-        nVertices = graph.vertexSet().size();
-        if (o == Operation.ENUMERATE) {
-            cycles = new ArrayList<>();
-        }
-        blocked = new boolean[nVertices];
-        stack = new ArrayDeque<>(nVertices);
+    /**
+     * Get the graph
+     *
+     * @return graph
+     */
+    public Graph<V, E> getGraph() {
+        return graph;
+    }
 
-        B = new ArrayList[nVertices];
-        for (int i = 0; i < nVertices; i++) {
-            // B[i] = new ArrayList<Integer>(nVertices);
-            B[i] = new ArrayList<>();
-        }
+    private void initState() {
+        iToV = new ArrayList<>(graph.vertexSet());
+        vToI = iToV.stream().collect(toMap(x -> x, iToV::indexOf));
 
-        iToV = (V[]) graph.vertexSet().toArray();
-        vToI = new HashMap<>();
-        for (int i = 0; i < iToV.length; i++) {
-            vToI.put(iToV[i], i);
-        }
+        blocked = new ArrayList<>(iToV.size());
+        blockedVertices = iToV.stream().map(v -> new ArrayList<Integer>()).collect(toList());
 
-        Ak = buildAdjacencyList();
+        stack = new ArrayDeque<>(iToV.size());
+        adjacencyList = buildAdjacencyList();
 
         stack.clear();
     }
 
-    @SuppressWarnings("unchecked")
-    private List<Integer>[] buildAdjacencyList()
-    {
-        @SuppressWarnings("rawtypes") List[] Ak = new ArrayList[nVertices];
-        for (int j = 0; j < nVertices; j++) {
-            V v = iToV[j];
-            List<V> s = Graphs.successorListOf(graph, v);
-            Ak[j] = new ArrayList<Integer>(s.size());
-
-            for (V value : s) {
-                Ak[j].add(vToI.get(value));
-            }
-        }
-
-        return Ak;
+    private List<List<Integer>> buildAdjacencyList() {
+        return iToV.stream()
+            .map(vertex -> Graphs.successorListOf(graph, vertex))
+            .map(successors -> successors.stream().map(s -> vToI.get(s)).collect(toList()))
+            .collect(toList());
     }
 
-    private void clearState()
-    {
-        Ak = null;
-        nVertices = 0;
-        blocked = null;
-        stack = null;
-        iToV = null;
-        vToI = null;
+    private void unblock(Integer u) {
+        blocked.add(u, false);
 
-        Ak = null;
-        B = null;
-    }
+        for (int wPos = 0; wPos < blockedVertices.get(u).size(); wPos++) {
+            Integer w = blockedVertices.get(u).get(wPos);
 
-    private boolean circuit(Integer v, Operation o)
-    {
-        boolean f = false;
+            wPos -= frequency(blockedVertices.get(u), u);
+            blockedVertices.get(u).removeAll(singletonList(u));
 
-        stack.push(v);
-        blocked[v] = true;
-
-        for (Integer w : Ak[v]) {
-            if (w < start) {
-                continue;
-            }
-
-            if (Objects.equals(w, start)) {
-                if (o == Operation.ENUMERATE) {
-                    List<V> cycle = new ArrayList<>(stack.size());
-
-                    for (Integer aStack : stack) {
-                        cycle.add(iToV[aStack]);
-                    }
-
-                    cycles.add(cycle);
-                }
-
-                if (o == Operation.PRINT_ONLY) {
-                    for (Integer i : stack) {
-                        System.out.print(iToV[i].toString() + " ");
-                    }
-                    System.out.println("");
-                }
-
-                nCycles++;
-
-                f = true;
-            } else if (!blocked[w]) {
-                if (circuit(w, o)) {
-                    f = true;
-                }
-            }
-        }
-
-        if (f) {
-            unblock(v);
-        } else {
-            for (Integer w : Ak[v]) {
-                if (w < start) {
-                    continue;
-                }
-
-                if (!B[w].contains(v)) {
-                    B[w].add(v);
-                }
-            }
-        }
-
-        stack.pop();
-
-        return f;
-    }
-
-    private void unblock(Integer u)
-    {
-        blocked[u] = false;
-
-        for (int wPos = 0; wPos < B[u].size(); wPos++) {
-            Integer w = B[u].get(wPos);
-
-            wPos -= removeFromList(B[u], w);
-
-            if (blocked[w]) {
+            if (blocked.get(w)) {
                 unblock(w);
             }
         }
     }
 
     /**
-     * Remove all occurrences of a value from the list.
-     *
-     * @param u the Integer to be removed.
-     * @param list the list from which all the occurrences of u must be removed.
-     */
-    private int removeFromList(List<Integer> list, Integer u)
-    {
-        int nOccurrences = 0;
-
-        Iterator<Integer> iterator = list.iterator();
-        while (iterator.hasNext()) {
-            Integer w = iterator.next();
-            if (Objects.equals(w, u)) {
-                nOccurrences++;
-                iterator.remove();
-            }
-        }
-
-        return nOccurrences;
-    }
-
-    /**
-     * Get the graph
-     * 
-     * @return graph
-     */
-    public Graph<V, E> getGraph()
-    {
-        return graph;
-    }
-
-    /**
-     * Set the graph
-     * 
-     * @param graph graph
-     */
-    public void setGraph(Graph<V, E> graph)
-    {
-        this.graph = GraphTests.requireDirected(graph, "Graph must be directed");
-    }
-
-    /**
      * {@inheritDoc}
      */
     @Override
-    public List<List<V>> findSimpleCycles()
-        throws IllegalArgumentException
-    {
-        if (graph == null) {
-            throw new IllegalArgumentException("Null graph.");
-        }
-
-        initState(Operation.ENUMERATE);
-
-        for (int i = 0; i < nVertices; i++) {
-            for (int j = 0; j < nVertices; j++) {
-                blocked[j] = false;
-                B[j].clear();
-            }
-
-            start = vToI.get(iToV[i]);
-            circuit(start, Operation.ENUMERATE);
-        }
-
-        List<List<V>> result = cycles;
-        clearState();
-        return result;
+    public List<List<V>> findSimpleCycles() throws IllegalArgumentException {
+        cycles = new ArrayList<>();
+        circuitAnalysis(() -> cycles.add(stack.stream().map(vi -> iToV.get(vi)).collect(toList())));
+        return cycles;
     }
 
     /**
      * Print to the standard output all simple cycles without building a list to keep them, thus
      * avoiding high memory consumption when investigating large and much connected graphs.
      */
-    public void printSimpleCycles()
-    {
-        if (graph == null) {
-            throw new IllegalArgumentException("Null graph.");
-        }
-
-        initState(Operation.PRINT_ONLY);
-
-        for (int i = 0; i < nVertices; i++) {
-            for (int j = 0; j < nVertices; j++) {
-                blocked[j] = false;
-                B[j].clear();
-            }
-
-            start = vToI.get(iToV[i]);
-            circuit(start, Operation.PRINT_ONLY);
-        }
-
-        clearState();
+    public void printSimpleCycles() {
+        circuitAnalysis(() -> {
+            stack.stream().map(i -> iToV.get(i).toString() + " ").forEach(System.out::print);
+            System.out.println();
+        });
     }
 
     /**
      * Count the number of simple cycles. It can count up to Long.MAX cycles in a graph.
-     * 
+     *
      * @return the number of simple cycles
      */
-    public long countSimpleCycles()
-    {
+    public long countSimpleCycles() {
+        nCycles = 0;
+        circuitAnalysis(() -> nCycles++);
+        return nCycles;
+    }
+
+    private boolean circuit(Integer vertex, int paths) {
+        boolean found = false;
+
+        stack.push(vertex);
+        blocked.add(vertex, true);
+
+        for (Integer adjacent : adjacencyList.get(vertex)) {
+            if (adjacent < start) {
+                continue;
+            }
+
+            if (Objects.equals(adjacent, start)) { // Cycle found
+                operation.run();
+                found = true;
+            } else if (!blocked.get(adjacent)) {
+                if (limitReached(paths) || circuit(adjacent, paths + 1)) {
+                    found = true;
+                }
+            }
+        }
+
+        if (found) {
+            unblock(vertex);
+        } else {
+            adjacencyList.get(vertex).stream()
+                .filter(adj -> adj >= start && !blockedVertices.get(adj).contains(vertex))
+                .forEach(adj -> blockedVertices.get(adj).add(vertex));
+        }
+
+        stack.pop();
+        return found;
+    }
+
+    private void circuitAnalysis(Runnable operation) {
         if (graph == null) {
             throw new IllegalArgumentException("Null graph.");
         }
 
-        initState(Operation.COUNT_ONLY);
+        this.operation = operation;
+        initState();
 
-        for (int i = 0; i < nVertices; i++) {
-            for (int j = 0; j < nVertices; j++) {
-                blocked[j] = false;
-                B[j].clear();
-            }
+        IntStream.range(0, iToV.size()).forEach(i -> {
+            blocked.clear();
+            iToV.forEach(j -> blocked.add(false));
+            IntStream.range(0, iToV.size()).forEach(j -> blockedVertices.get(j).clear());
 
-            start = vToI.get(iToV[i]);
-            circuit(start, Operation.COUNT_ONLY);
-        }
+            start = i;
+            circuit(start, 0);
+        });
+    }
 
-        clearState();
+    /**
+     * Limits the maximum number of edges in a cycle.
+     *
+     * @param pathLimit maximum paths.
+     */
+    public void setPathLimit(int pathLimit) {
+        this.pathLimit = pathLimit - 1;
+        this.hasLimit = true;
+    }
 
-        return nCycles;
+    /**
+     * This is the default behaviour of the algorithm.
+     * It will keep looking as long as there are paths available.
+     */
+    public void unlimitedPaths() {
+        this.hasLimit = false;
+    }
+
+    private boolean limitReached(int steps) {
+        return hasLimit && steps >= pathLimit;
     }
 }
