675dd2681634bfff4539d3e7d7e3930e8b560a87
aalhossary <aalhossary@gmil.com>
2019-11-06 10:56:26 +0800

    Bollobas Directed scale-free graph generator
    Code and unit test

---
+++ jgrapht-core/src/test/java/org/jgrapht/generate/BollobasGraphGeneratorTest.java
@@ -0,0 +1,77 @@
+package org.jgrapht.generate;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.util.SupplierUtil;
+import org.junit.Test;
+
+public class BollobasGraphGeneratorTest
+{
+    @Test
+    public void testBadParameters()
+    {
+        try {
+            new BollobasGraphGenerator<>(-0.5f, 0.33f, 0.5f, 0.5f, 500, 500);
+            fail("Bad alpha checking");
+        } catch (IllegalArgumentException e) {
+        }
+        try {
+            new BollobasGraphGenerator<>(0.33f, -0.5f, 0.5f, 0.5f, 500, 500);
+            fail("Bad gamma checking");
+        } catch (IllegalArgumentException e) {
+        }
+        try {
+            new BollobasGraphGenerator<>(0.66f, 0.66f, 0.5f, 0.5f, 500, 500);
+            fail("Bad alpha + gamma checking");
+        } catch (IllegalArgumentException e) {
+        }
+        try {
+            new BollobasGraphGenerator<>(0.33f, 0.33f, -0.5f, 0.5f, 500, 500);
+            fail("Bad deltaIn checking");
+        } catch (IllegalArgumentException e) {
+        }
+        try {
+            new BollobasGraphGenerator<>(0.33f, 0.33f, 0.5f, -0.5f, 500, 500);
+            fail("Bad deltaOut checking");
+        } catch (IllegalArgumentException e) {
+        }
+        try {
+            new BollobasGraphGenerator<>(0.33f, 0.33f, 0.5f, 0.5f, -1, -1);
+            fail("Bad target checking");
+        } catch (IllegalArgumentException e) {
+        }
+        try {
+            new BollobasGraphGenerator<>(0.33f, 0.33f, 0.5f, 0.5f, 500, 500, null);
+            fail("Bad random number generator checking");
+        } catch (NullPointerException e) {
+        }
+
+    }
+
+    @Test
+    public void testNumberOfEdges()
+    {
+        GraphGenerator<Integer, DefaultEdge, Integer> generator =
+            new BollobasGraphGenerator<>(0.33f, 0.33f, 0.5f, 0.5f, 500, 0);
+        Graph<Integer, DefaultEdge> g = new DefaultDirectedGraph<>(
+            SupplierUtil.createIntegerSupplier(), SupplierUtil.DEFAULT_EDGE_SUPPLIER, false);
+        generator.generateGraph(g);
+        assertEquals(500, g.edgeSet().size());
+    }
+
+    @Test
+    public void testNumberOfNodes()
+    {
+        GraphGenerator<Integer, DefaultEdge, Integer> generator =
+            new BollobasGraphGenerator<>(0.33f, 0.33f, 0.5f, 0.5f, -1, 500);
+        Graph<Integer, DefaultEdge> g = new DefaultDirectedGraph<>(
+            SupplierUtil.createIntegerSupplier(), SupplierUtil.DEFAULT_EDGE_SUPPLIER, false);
+        generator.generateGraph(g);
+        assertEquals(500, g.vertexSet().size());
+    }
+
+}
