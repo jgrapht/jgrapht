3910985339567453bb37a56683dba288f0591467
Bingo-Soft <bingosoft@bk.ru>
2019-11-19 18:42:08 +0300

    test: improve GraphWalk test of equality

--- jgrapht-core/src/test/java/org/jgrapht/graph/GraphWalkTest.java
+++ jgrapht-core/src/test/java/org/jgrapht/graph/GraphWalkTest.java
@@ -307,13 +307,11 @@ public class GraphWalkTest
     public void testFirstEmptyWalkEquality()
     {
         Graph<Integer, DefaultEdge> graph1 = new SimpleGraph<>(DefaultEdge.class);
-        GraphWalk<Integer, DefaultEdge> gw1 =
-            new GraphWalk<>(graph1, null, null, null, Collections.emptyList(), 0);
+        GraphWalk<Integer,DefaultEdge> gw1 = GraphWalk.emptyWalk(graph1);
 
         Graph<Integer, DefaultEdge> graph2 = new SimpleGraph<>(DefaultEdge.class);
         graph2.addVertex(0);
-        GraphWalk<Integer, DefaultEdge> gw2 =
-            new GraphWalk<>(graph2, 0, 0, Collections.singletonList(0), Collections.emptyList(), 0);
+        GraphWalk<Integer,DefaultEdge> gw2 = GraphWalk.singletonWalk(graph2, 0);
         Assert.assertNotEquals(gw1, gw2);
     }
 
