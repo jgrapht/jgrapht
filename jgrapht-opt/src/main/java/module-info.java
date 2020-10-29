8f743c3e79296a1d45c3c43db50408da093abbf7
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2020-10-29 17:38:36 +0100

    Fixed module-info.java

--- jgrapht-opt/src/main/java/module-info.java
+++ jgrapht-opt/src/main/java/module-info.java
@@ -2,12 +2,12 @@ module org.jgrapht.opt
 {
     exports org.jgrapht.opt.graph.fastutil;
     exports org.jgrapht.opt.graph.sparse;
-    
+	exports org.jgrapht.opt.graph.webgraph;
+
     requires transitive org.jgrapht.core;
     requires transitive it.unimi.dsi.fastutil;
-	requires webgraph.big;
-	requires com.google.common;
-	requires dsiutils;
-	requires webgraph;
-   
+	requires transitive webgraph;
+	requires transitive dsiutils;
+	requires transitive webgraph.big;
+	requires transitive com.google.common;
 }
