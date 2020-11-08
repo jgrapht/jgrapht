45851e97e8cc1fab6020ea97cce93b3fde9167f0
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2020-11-08 23:44:24 +0100

    Completed integration with DSI stuff

--- jgrapht-opt/src/main/java/module-info.java
+++ jgrapht-opt/src/main/java/module-info.java
@@ -7,7 +7,7 @@ module org.jgrapht.opt
     requires transitive org.jgrapht.core;
     requires transitive it.unimi.dsi.fastutil;
 	requires transitive it.unimi.dsi.webgraph;
-	requires transitive webgraph.big;
+	requires transitive it.unimi.dsi.big.webgraph;
 	requires transitive it.unimi.dsi.dsiutils;
 	requires transitive it.unimi.dsi.sux4j;
 	requires transitive com.google.common;
