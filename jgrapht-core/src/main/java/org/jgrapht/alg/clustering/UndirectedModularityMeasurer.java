d3eda95404d6810f1daac1742a5b7df9526d5194
Dimitrios Michail <dimitrios.michail@gmail.com>
2021-05-07 19:30:15 +0300

    Fixed checkstyle issues

--- jgrapht-core/src/main/java/org/jgrapht/alg/clustering/UndirectedModularityMeasurer.java
+++ jgrapht-core/src/main/java/org/jgrapht/alg/clustering/UndirectedModularityMeasurer.java
@@ -28,125 +28,126 @@ import org.jgrapht.GraphTests;
 import org.jgrapht.Graphs;
 
 /**
- * A
- * <a href="https://en.wikipedia.org/wiki/Modularity_(networks)">modularity</a>
- * measurer.
+ * A <a href="https://en.wikipedia.org/wiki/Modularity_(networks)">modularity</a> measurer.
  * 
  * <p>
- * This is a utility class which computes the modularity function. It takes as
- * input a list of vertex classes $C$ and a graph $G$ and calculates: $Q =
- * \frac{1}{2m} \sum_{ij} \left( A_{ij} - \frac{k_i k_j}{2m} \right) \delta(C_i,
- * C_j)$. Here $m$ is the total number of edges and $k_i$ is the degree of
- * vertex $i$. $A_{ij}$ is either $1$ or $0$ depending on whether edge $(i,j)$
- * belongs to the graph and $\delta(C_i, C_j)$ is 1 if vertices $i$ and $j$
- * belong to the same class, $0$ otherwise.
+ * This is a utility class which computes the modularity function. It takes as input a list of
+ * vertex classes $C$ and a graph $G$ and calculates: $Q = \frac{1}{2m} \sum_{ij} \left( A_{ij} -
+ * \frac{k_i k_j}{2m} \right) \delta(C_i, C_j)$. Here $m$ is the total number of edges and $k_i$ is
+ * the degree of vertex $i$. $A_{ij}$ is either $1$ or $0$ depending on whether edge $(i,j)$ belongs
+ * to the graph and $\delta(C_i, C_j)$ is 1 if vertices $i$ and $j$ belong to the same class, $0$
+ * otherwise.
  * 
  * @author Dimitrios Michail
  *
  * @param <V> the vertex type
  * @param <E> the edge type
  */
-public class UndirectedModularityMeasurer<V, E> {
+public class UndirectedModularityMeasurer<V, E>
+{
 
-	private static final String INVALID_PARTITION_OF_VERTICES = "Invalid partition of vertices";
-	private final Graph<V, E> graph;
-	private double m;
-	private Map<V, Double> degrees;
+    private static final String INVALID_PARTITION_OF_VERTICES = "Invalid partition of vertices";
+    private final Graph<V, E> graph;
+    private double m;
+    private Map<V, Double> degrees;
 
-	/**
-	 * Construct a new measurer
-	 * 
-	 * @param graph the input graph
-	 */
-	public UndirectedModularityMeasurer(Graph<V, E> graph) {
-		this.graph = GraphTests.requireUndirected(graph);
-		this.degrees = new HashMap<>();
-		precomputeDegrees(graph);
-	}
+    /**
+     * Construct a new measurer
+     * 
+     * @param graph the input graph
+     */
+    public UndirectedModularityMeasurer(Graph<V, E> graph)
+    {
+        this.graph = GraphTests.requireUndirected(graph);
+        this.degrees = new HashMap<>();
+        precomputeDegrees(graph);
+    }
 
-	/**
-	 * Compute the modularity of a vertex partition.
-	 * 
-	 * @param partitions the partitions
-	 * @return the modularity
-	 */
-	public double modularity(List<Set<V>> partitions) {
-		// index partitions and count total (weighted) degree inside each partition
-		int totalPartitions = partitions.size();
-		Map<V, Integer> vertexPartition = new HashMap<>();
-		double[] weightedDegreeInPartition = new double[totalPartitions];
-		int curPartition = 0;
-		for (Set<V> partition : partitions) {
-			weightedDegreeInPartition[curPartition] = 0d;
-			for (V v : partition) {
-				vertexPartition.put(v, curPartition);
-				Double d = degrees.get(v);
-				if (d == null) {
-					throw new IllegalArgumentException(INVALID_PARTITION_OF_VERTICES);
-				}
-				weightedDegreeInPartition[curPartition] += d;
-			}
-			curPartition++;
-		}
+    /**
+     * Compute the modularity of a vertex partition.
+     * 
+     * @param partitions the partitions
+     * @return the modularity
+     */
+    public double modularity(List<Set<V>> partitions)
+    {
+        // index partitions and count total (weighted) degree inside each partition
+        int totalPartitions = partitions.size();
+        Map<V, Integer> vertexPartition = new HashMap<>();
+        double[] weightedDegreeInPartition = new double[totalPartitions];
+        int curPartition = 0;
+        for (Set<V> partition : partitions) {
+            weightedDegreeInPartition[curPartition] = 0d;
+            for (V v : partition) {
+                vertexPartition.put(v, curPartition);
+                Double d = degrees.get(v);
+                if (d == null) {
+                    throw new IllegalArgumentException(INVALID_PARTITION_OF_VERTICES);
+                }
+                weightedDegreeInPartition[curPartition] += d;
+            }
+            curPartition++;
+        }
 
-		// count (weighted) edges inside each partition
-		double[] edgeWeightInPartition = new double[totalPartitions];
-		for (E e : graph.edgeSet()) {
-			V v = graph.getEdgeSource(e);
-			V u = graph.getEdgeTarget(e);
-			Integer pv = vertexPartition.get(v);
-			if (pv == null) {
-				throw new IllegalArgumentException(INVALID_PARTITION_OF_VERTICES);
-			}
-			Integer pu = vertexPartition.get(u);
-			if (pu == null) {
-				throw new IllegalArgumentException(INVALID_PARTITION_OF_VERTICES);
-			}
-			if (pv.intValue() == pu.intValue()) {
-				// same partition
-				edgeWeightInPartition[pv] += graph.getEdgeWeight(e);
-			}
-		}
+        // count (weighted) edges inside each partition
+        double[] edgeWeightInPartition = new double[totalPartitions];
+        for (E e : graph.edgeSet()) {
+            V v = graph.getEdgeSource(e);
+            V u = graph.getEdgeTarget(e);
+            Integer pv = vertexPartition.get(v);
+            if (pv == null) {
+                throw new IllegalArgumentException(INVALID_PARTITION_OF_VERTICES);
+            }
+            Integer pu = vertexPartition.get(u);
+            if (pu == null) {
+                throw new IllegalArgumentException(INVALID_PARTITION_OF_VERTICES);
+            }
+            if (pv.intValue() == pu.intValue()) {
+                // same partition
+                edgeWeightInPartition[pv] += graph.getEdgeWeight(e);
+            }
+        }
 
-		// compute modularity summing over partitions
-		double mod = 0d;
-		for (int p = 0; p < totalPartitions; p++) {
-			double expectedEdgeWeightInPartition = weightedDegreeInPartition[p] * weightedDegreeInPartition[p]
-					/ (2d * m);
-			mod += 2d * edgeWeightInPartition[p] - expectedEdgeWeightInPartition;
-		}
-		mod /= 2d * m;
+        // compute modularity summing over partitions
+        double mod = 0d;
+        for (int p = 0; p < totalPartitions; p++) {
+            double expectedEdgeWeightInPartition =
+                weightedDegreeInPartition[p] * weightedDegreeInPartition[p] / (2d * m);
+            mod += 2d * edgeWeightInPartition[p] - expectedEdgeWeightInPartition;
+        }
+        mod /= 2d * m;
 
-		return mod;
-	}
+        return mod;
+    }
 
-	/**
-	 * Pre-compute vertex (weighted) degrees.
-	 * 
-	 * @param graph the input graph
-	 */
-	private void precomputeDegrees(Graph<V, E> graph) {
-		if (graph.getType().isWeighted()) {
-			m = graph.edgeSet().stream().collect(Collectors.summingDouble(graph::getEdgeWeight));
-			for (V v : graph.vertexSet()) {
-				double sum = 0d;
-				for (E e : graph.outgoingEdgesOf(v)) {
-					V u = Graphs.getOppositeVertex(graph, e, v);
-					if (u.equals(v)) {
-						sum += 2d * graph.getEdgeWeight(e);
-					} else {
-						sum += graph.getEdgeWeight(e);
-					}
-				}
-				degrees.put(v, sum);
-			}
-		} else {
-			m = graph.edgeSet().size();
-			for (V v : graph.vertexSet()) {
-				// degreeof counts loops twice anyway
-				degrees.put(v, Double.valueOf(graph.degreeOf(v)));
-			}
-		}
-	}
+    /**
+     * Pre-compute vertex (weighted) degrees.
+     * 
+     * @param graph the input graph
+     */
+    private void precomputeDegrees(Graph<V, E> graph)
+    {
+        if (graph.getType().isWeighted()) {
+            m = graph.edgeSet().stream().collect(Collectors.summingDouble(graph::getEdgeWeight));
+            for (V v : graph.vertexSet()) {
+                double sum = 0d;
+                for (E e : graph.outgoingEdgesOf(v)) {
+                    V u = Graphs.getOppositeVertex(graph, e, v);
+                    if (u.equals(v)) {
+                        sum += 2d * graph.getEdgeWeight(e);
+                    } else {
+                        sum += graph.getEdgeWeight(e);
+                    }
+                }
+                degrees.put(v, sum);
+            }
+        } else {
+            m = graph.edgeSet().size();
+            for (V v : graph.vertexSet()) {
+                // degreeof counts loops twice anyway
+                degrees.put(v, Double.valueOf(graph.degreeOf(v)));
+            }
+        }
+    }
 
 }
--- jgrapht-core/src/test/java/org/jgrapht/alg/clustering/UndirectedModularityMeasurerTest.java
+++ jgrapht-core/src/test/java/org/jgrapht/alg/clustering/UndirectedModularityMeasurerTest.java
@@ -33,302 +33,335 @@ import org.junit.Test;
  *
  * @author Dimitrios Michail
  */
-public class UndirectedModularityMeasurerTest {
+public class UndirectedModularityMeasurerTest
+{
 
-	@Test
-	public void testOptimalPartition() {
-		Graph<Integer, DefaultWeightedEdge> g = GraphTypeBuilder.undirected().allowingMultipleEdges(true)
-				.allowingSelfLoops(true).weighted(true).edgeSupplier(SupplierUtil.DEFAULT_WEIGHTED_EDGE_SUPPLIER)
-				.vertexSupplier(SupplierUtil.createIntegerSupplier()).buildGraph();
+    @Test
+    public void testOptimalPartition()
+    {
+        Graph<Integer,
+            DefaultWeightedEdge> g = GraphTypeBuilder
+                .undirected().allowingMultipleEdges(true).allowingSelfLoops(true).weighted(true)
+                .edgeSupplier(SupplierUtil.DEFAULT_WEIGHTED_EDGE_SUPPLIER)
+                .vertexSupplier(SupplierUtil.createIntegerSupplier()).buildGraph();
 
-		g.addVertex(0);
-		g.addVertex(1);
-		g.addVertex(2);
-		g.addVertex(3);
-		g.addVertex(4);
+        g.addVertex(0);
+        g.addVertex(1);
+        g.addVertex(2);
+        g.addVertex(3);
+        g.addVertex(4);
 
-		g.addVertex(5);
-		g.addVertex(6);
-		g.addVertex(7);
-		g.addVertex(8);
+        g.addVertex(5);
+        g.addVertex(6);
+        g.addVertex(7);
+        g.addVertex(8);
 
-		g.addEdge(0, 1);
-		g.addEdge(0, 4);
-		g.addEdge(1, 2);
-		g.addEdge(1, 3);
-		g.addEdge(1, 5);
-		g.addEdge(2, 3);
-		g.addEdge(2, 4);
-		g.addEdge(3, 4);
+        g.addEdge(0, 1);
+        g.addEdge(0, 4);
+        g.addEdge(1, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 5);
+        g.addEdge(2, 3);
+        g.addEdge(2, 4);
+        g.addEdge(3, 4);
 
-		g.addEdge(5, 6);
-		g.addEdge(5, 7);
-		g.addEdge(5, 8);
-		g.addEdge(6, 7);
-		g.addEdge(7, 8);
+        g.addEdge(5, 6);
+        g.addEdge(5, 7);
+        g.addEdge(5, 8);
+        g.addEdge(6, 7);
+        g.addEdge(7, 8);
 
-		UndirectedModularityMeasurer<Integer, DefaultWeightedEdge> measurer = new UndirectedModularityMeasurer<>(g);
+        UndirectedModularityMeasurer<Integer, DefaultWeightedEdge> measurer =
+            new UndirectedModularityMeasurer<>(g);
 
-		List<Set<Integer>> partitions = List.of(Set.of(0, 1, 2, 3, 4), Set.of(5, 6, 7, 8));
-		double mod = measurer.modularity(partitions);
+        List<Set<Integer>> partitions = List.of(Set.of(0, 1, 2, 3, 4), Set.of(5, 6, 7, 8));
+        double mod = measurer.modularity(partitions);
 
-		assertEquals(mod, 0.4112426, 1e-6);
+        assertEquals(mod, 0.4112426, 1e-6);
 
-	}
+    }
 
-	@Test
-	public void testSingle() {
-		Graph<Integer, DefaultWeightedEdge> g = GraphTypeBuilder.undirected().allowingMultipleEdges(true)
-				.allowingSelfLoops(true).weighted(true).edgeSupplier(SupplierUtil.DEFAULT_WEIGHTED_EDGE_SUPPLIER)
-				.vertexSupplier(SupplierUtil.createIntegerSupplier()).buildGraph();
+    @Test
+    public void testSingle()
+    {
+        Graph<Integer,
+            DefaultWeightedEdge> g = GraphTypeBuilder
+                .undirected().allowingMultipleEdges(true).allowingSelfLoops(true).weighted(true)
+                .edgeSupplier(SupplierUtil.DEFAULT_WEIGHTED_EDGE_SUPPLIER)
+                .vertexSupplier(SupplierUtil.createIntegerSupplier()).buildGraph();
 
-		g.addVertex(0);
-		g.addVertex(1);
-		g.addVertex(2);
-		g.addVertex(3);
-		g.addVertex(4);
+        g.addVertex(0);
+        g.addVertex(1);
+        g.addVertex(2);
+        g.addVertex(3);
+        g.addVertex(4);
 
-		g.addVertex(5);
-		g.addVertex(6);
-		g.addVertex(7);
-		g.addVertex(8);
+        g.addVertex(5);
+        g.addVertex(6);
+        g.addVertex(7);
+        g.addVertex(8);
 
-		g.addEdge(0, 1);
-		g.addEdge(0, 4);
-		g.addEdge(1, 2);
-		g.addEdge(1, 3);
-		g.addEdge(1, 5);
-		g.addEdge(2, 3);
-		g.addEdge(2, 4);
-		g.addEdge(3, 4);
+        g.addEdge(0, 1);
+        g.addEdge(0, 4);
+        g.addEdge(1, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 5);
+        g.addEdge(2, 3);
+        g.addEdge(2, 4);
+        g.addEdge(3, 4);
 
-		g.addEdge(5, 6);
-		g.addEdge(5, 7);
-		g.addEdge(5, 8);
-		g.addEdge(6, 7);
-		g.addEdge(7, 8);
+        g.addEdge(5, 6);
+        g.addEdge(5, 7);
+        g.addEdge(5, 8);
+        g.addEdge(6, 7);
+        g.addEdge(7, 8);
 
-		UndirectedModularityMeasurer<Integer, DefaultWeightedEdge> measurer = new UndirectedModularityMeasurer<>(g);
+        UndirectedModularityMeasurer<Integer, DefaultWeightedEdge> measurer =
+            new UndirectedModularityMeasurer<>(g);
 
-		List<Set<Integer>> partitions = List.of(Set.of(0, 1, 2, 3, 4, 5, 6, 7, 8));
-		double mod = measurer.modularity(partitions);
+        List<Set<Integer>> partitions = List.of(Set.of(0, 1, 2, 3, 4, 5, 6, 7, 8));
+        double mod = measurer.modularity(partitions);
 
-		assertEquals(mod, 0.0, 1e-6);
+        assertEquals(mod, 0.0, 1e-6);
 
-	}
+    }
 
-	@Test
-	public void testSuboptimal() {
-		Graph<Integer, DefaultWeightedEdge> g = GraphTypeBuilder.undirected().allowingMultipleEdges(true)
-				.allowingSelfLoops(true).weighted(true).edgeSupplier(SupplierUtil.DEFAULT_WEIGHTED_EDGE_SUPPLIER)
-				.vertexSupplier(SupplierUtil.createIntegerSupplier()).buildGraph();
+    @Test
+    public void testSuboptimal()
+    {
+        Graph<Integer,
+            DefaultWeightedEdge> g = GraphTypeBuilder
+                .undirected().allowingMultipleEdges(true).allowingSelfLoops(true).weighted(true)
+                .edgeSupplier(SupplierUtil.DEFAULT_WEIGHTED_EDGE_SUPPLIER)
+                .vertexSupplier(SupplierUtil.createIntegerSupplier()).buildGraph();
 
-		g.addVertex(0);
-		g.addVertex(1);
-		g.addVertex(2);
-		g.addVertex(3);
-		g.addVertex(4);
+        g.addVertex(0);
+        g.addVertex(1);
+        g.addVertex(2);
+        g.addVertex(3);
+        g.addVertex(4);
 
-		g.addVertex(5);
-		g.addVertex(6);
-		g.addVertex(7);
-		g.addVertex(8);
+        g.addVertex(5);
+        g.addVertex(6);
+        g.addVertex(7);
+        g.addVertex(8);
 
-		g.addEdge(0, 1);
-		g.addEdge(0, 4);
-		g.addEdge(1, 2);
-		g.addEdge(1, 3);
-		g.addEdge(1, 5);
-		g.addEdge(2, 3);
-		g.addEdge(2, 4);
-		g.addEdge(3, 4);
+        g.addEdge(0, 1);
+        g.addEdge(0, 4);
+        g.addEdge(1, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 5);
+        g.addEdge(2, 3);
+        g.addEdge(2, 4);
+        g.addEdge(3, 4);
 
-		g.addEdge(5, 6);
-		g.addEdge(5, 7);
-		g.addEdge(5, 8);
-		g.addEdge(6, 7);
-		g.addEdge(7, 8);
+        g.addEdge(5, 6);
+        g.addEdge(5, 7);
+        g.addEdge(5, 8);
+        g.addEdge(6, 7);
+        g.addEdge(7, 8);
 
-		UndirectedModularityMeasurer<Integer, DefaultWeightedEdge> measurer = new UndirectedModularityMeasurer<>(g);
+        UndirectedModularityMeasurer<Integer, DefaultWeightedEdge> measurer =
+            new UndirectedModularityMeasurer<>(g);
 
-		List<Set<Integer>> partitions = List.of(Set.of(0, 3, 4), Set.of(1, 2, 5, 6, 7, 8));
-		double mod = measurer.modularity(partitions);
+        List<Set<Integer>> partitions = List.of(Set.of(0, 3, 4), Set.of(1, 2, 5, 6, 7, 8));
+        double mod = measurer.modularity(partitions);
 
-		assertEquals(mod, 0.118343, 1e-6);
+        assertEquals(mod, 0.118343, 1e-6);
 
-	}
+    }
 
-	@Test
-	public void testNegative() {
-		Graph<Integer, DefaultWeightedEdge> g = GraphTypeBuilder.undirected().allowingMultipleEdges(true)
-				.allowingSelfLoops(true).weighted(true).edgeSupplier(SupplierUtil.DEFAULT_WEIGHTED_EDGE_SUPPLIER)
-				.vertexSupplier(SupplierUtil.createIntegerSupplier()).buildGraph();
+    @Test
+    public void testNegative()
+    {
+        Graph<Integer,
+            DefaultWeightedEdge> g = GraphTypeBuilder
+                .undirected().allowingMultipleEdges(true).allowingSelfLoops(true).weighted(true)
+                .edgeSupplier(SupplierUtil.DEFAULT_WEIGHTED_EDGE_SUPPLIER)
+                .vertexSupplier(SupplierUtil.createIntegerSupplier()).buildGraph();
 
-		g.addVertex(0);
-		g.addVertex(1);
-		g.addVertex(2);
-		g.addVertex(3);
-		g.addVertex(4);
+        g.addVertex(0);
+        g.addVertex(1);
+        g.addVertex(2);
+        g.addVertex(3);
+        g.addVertex(4);
 
-		g.addVertex(5);
-		g.addVertex(6);
-		g.addVertex(7);
-		g.addVertex(8);
+        g.addVertex(5);
+        g.addVertex(6);
+        g.addVertex(7);
+        g.addVertex(8);
 
-		g.addEdge(0, 1);
-		g.addEdge(0, 4);
-		g.addEdge(1, 2);
-		g.addEdge(1, 3);
-		g.addEdge(1, 5);
-		g.addEdge(2, 3);
-		g.addEdge(2, 4);
-		g.addEdge(3, 4);
+        g.addEdge(0, 1);
+        g.addEdge(0, 4);
+        g.addEdge(1, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 5);
+        g.addEdge(2, 3);
+        g.addEdge(2, 4);
+        g.addEdge(3, 4);
 
-		g.addEdge(5, 6);
-		g.addEdge(5, 7);
-		g.addEdge(5, 8);
-		g.addEdge(6, 7);
-		g.addEdge(7, 8);
+        g.addEdge(5, 6);
+        g.addEdge(5, 7);
+        g.addEdge(5, 8);
+        g.addEdge(6, 7);
+        g.addEdge(7, 8);
 
-		UndirectedModularityMeasurer<Integer, DefaultWeightedEdge> measurer = new UndirectedModularityMeasurer<>(g);
+        UndirectedModularityMeasurer<Integer, DefaultWeightedEdge> measurer =
+            new UndirectedModularityMeasurer<>(g);
 
-		List<Set<Integer>> partitions = List.of(Set.of(0), Set.of(3), Set.of(4), Set.of(1), Set.of(2), Set.of(5),
-				Set.of(6), Set.of(7), Set.of(8));
-		double mod = measurer.modularity(partitions);
+        List<Set<Integer>> partitions = List
+            .of(
+                Set.of(0), Set.of(3), Set.of(4), Set.of(1), Set.of(2), Set.of(5), Set.of(6),
+                Set.of(7), Set.of(8));
+        double mod = measurer.modularity(partitions);
 
-		assertEquals(mod, -0.118343, 1e-6);
+        assertEquals(mod, -0.118343, 1e-6);
 
-	}
+    }
 
-	@Test
-	public void test24() {
-		Graph<Integer, DefaultWeightedEdge> g = GraphTypeBuilder.undirected().allowingMultipleEdges(true)
-				.allowingSelfLoops(true).weighted(true).edgeSupplier(SupplierUtil.DEFAULT_WEIGHTED_EDGE_SUPPLIER)
-				.vertexSupplier(SupplierUtil.createIntegerSupplier()).buildGraph();
+    @Test
+    public void test24()
+    {
+        Graph<Integer,
+            DefaultWeightedEdge> g = GraphTypeBuilder
+                .undirected().allowingMultipleEdges(true).allowingSelfLoops(true).weighted(true)
+                .edgeSupplier(SupplierUtil.DEFAULT_WEIGHTED_EDGE_SUPPLIER)
+                .vertexSupplier(SupplierUtil.createIntegerSupplier()).buildGraph();
 
-		for (int i = 0; i < 16; i++) {
-			g.addVertex(i);
-		}
+        for (int i = 0; i < 16; i++) {
+            g.addVertex(i);
+        }
 
-		g.addEdge(0, 1);
-		g.addEdge(0, 2);
-		g.addEdge(0, 4);
-		g.addEdge(1, 2);
-		g.addEdge(2, 3);
-		g.addEdge(2, 4);
-		g.addEdge(2, 5);
-		g.addEdge(2, 8);
-		g.addEdge(3, 4);
-		g.addEdge(3, 6);
-		g.addEdge(5, 6);
-		g.addEdge(5, 7);
-		g.addEdge(6, 7);
-		g.addEdge(8, 9);
-		g.addEdge(8, 10);
-		g.addEdge(9, 10);
-		g.addEdge(9, 11);
-		g.addEdge(10, 11);
-		g.addEdge(11, 12);
-		g.addEdge(12, 13);
-		g.addEdge(12, 14);
-		g.addEdge(13, 14);
-		g.addEdge(13, 15);
-		g.addEdge(14, 15);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(0, 4);
+        g.addEdge(1, 2);
+        g.addEdge(2, 3);
+        g.addEdge(2, 4);
+        g.addEdge(2, 5);
+        g.addEdge(2, 8);
+        g.addEdge(3, 4);
+        g.addEdge(3, 6);
+        g.addEdge(5, 6);
+        g.addEdge(5, 7);
+        g.addEdge(6, 7);
+        g.addEdge(8, 9);
+        g.addEdge(8, 10);
+        g.addEdge(9, 10);
+        g.addEdge(9, 11);
+        g.addEdge(10, 11);
+        g.addEdge(11, 12);
+        g.addEdge(12, 13);
+        g.addEdge(12, 14);
+        g.addEdge(13, 14);
+        g.addEdge(13, 15);
+        g.addEdge(14, 15);
 
-		UndirectedModularityMeasurer<Integer, DefaultWeightedEdge> measurer = new UndirectedModularityMeasurer<>(g);
+        UndirectedModularityMeasurer<Integer, DefaultWeightedEdge> measurer =
+            new UndirectedModularityMeasurer<>(g);
 
-		List<Set<Integer>> partitions = List.of(Set.of(0, 1, 2, 3, 4), Set.of(5, 6, 7), Set.of(8, 9, 10, 11),
-				Set.of(12, 13, 14, 15));
-		double mod = measurer.modularity(partitions);
+        List<Set<Integer>> partitions = List
+            .of(
+                Set.of(0, 1, 2, 3, 4), Set.of(5, 6, 7), Set.of(8, 9, 10, 11),
+                Set.of(12, 13, 14, 15));
+        double mod = measurer.modularity(partitions);
 
-		assertEquals(0.565104, mod, 1e-6);
+        assertEquals(0.565104, mod, 1e-6);
 
-	}
+    }
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testWithInvalidPartition() {
-		Graph<Integer, DefaultWeightedEdge> g = GraphTypeBuilder.undirected().allowingMultipleEdges(true)
-				.allowingSelfLoops(true).weighted(true).edgeSupplier(SupplierUtil.DEFAULT_WEIGHTED_EDGE_SUPPLIER)
-				.vertexSupplier(SupplierUtil.createIntegerSupplier()).buildGraph();
+    @Test(expected = IllegalArgumentException.class)
+    public void testWithInvalidPartition()
+    {
+        Graph<Integer,
+            DefaultWeightedEdge> g = GraphTypeBuilder
+                .undirected().allowingMultipleEdges(true).allowingSelfLoops(true).weighted(true)
+                .edgeSupplier(SupplierUtil.DEFAULT_WEIGHTED_EDGE_SUPPLIER)
+                .vertexSupplier(SupplierUtil.createIntegerSupplier()).buildGraph();
 
-		g.addVertex(0);
-		g.addVertex(1);
-		g.addVertex(2);
-		g.addVertex(3);
-		g.addVertex(4);
+        g.addVertex(0);
+        g.addVertex(1);
+        g.addVertex(2);
+        g.addVertex(3);
+        g.addVertex(4);
 
-		g.addVertex(5);
-		g.addVertex(6);
-		g.addVertex(7);
-		g.addVertex(8);
+        g.addVertex(5);
+        g.addVertex(6);
+        g.addVertex(7);
+        g.addVertex(8);
 
-		g.addEdge(0, 1);
-		g.addEdge(0, 4);
-		g.addEdge(1, 2);
-		g.addEdge(1, 3);
-		g.addEdge(1, 5);
-		g.addEdge(2, 3);
-		g.addEdge(2, 4);
-		g.addEdge(3, 4);
+        g.addEdge(0, 1);
+        g.addEdge(0, 4);
+        g.addEdge(1, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 5);
+        g.addEdge(2, 3);
+        g.addEdge(2, 4);
+        g.addEdge(3, 4);
 
-		g.addEdge(5, 6);
-		g.addEdge(5, 7);
-		g.addEdge(5, 8);
-		g.addEdge(6, 7);
-		g.addEdge(7, 8);
+        g.addEdge(5, 6);
+        g.addEdge(5, 7);
+        g.addEdge(5, 8);
+        g.addEdge(6, 7);
+        g.addEdge(7, 8);
 
-		UndirectedModularityMeasurer<Integer, DefaultWeightedEdge> measurer = new UndirectedModularityMeasurer<>(g);
+        UndirectedModularityMeasurer<Integer, DefaultWeightedEdge> measurer =
+            new UndirectedModularityMeasurer<>(g);
 
-		List<Set<Integer>> partitions = List.of(Set.of(0, 3, 4), Set.of(1, 2, 5, 7, 8, 9));
-		double mod = measurer.modularity(partitions);
+        List<Set<Integer>> partitions = List.of(Set.of(0, 3, 4), Set.of(1, 2, 5, 7, 8, 9));
+        double mod = measurer.modularity(partitions);
 
-		assertEquals(mod, 0.118343, 1e-6);
+        assertEquals(mod, 0.118343, 1e-6);
 
-	}
+    }
 
-	@Test
-	public void testWithSelfLoops() {
-		Graph<Integer, DefaultWeightedEdge> g = GraphTypeBuilder.undirected().allowingMultipleEdges(true)
-				.allowingSelfLoops(true).weighted(true).edgeSupplier(SupplierUtil.DEFAULT_WEIGHTED_EDGE_SUPPLIER)
-				.vertexSupplier(SupplierUtil.createIntegerSupplier()).buildGraph();
+    @Test
+    public void testWithSelfLoops()
+    {
+        Graph<Integer,
+            DefaultWeightedEdge> g = GraphTypeBuilder
+                .undirected().allowingMultipleEdges(true).allowingSelfLoops(true).weighted(true)
+                .edgeSupplier(SupplierUtil.DEFAULT_WEIGHTED_EDGE_SUPPLIER)
+                .vertexSupplier(SupplierUtil.createIntegerSupplier()).buildGraph();
 
-		g.addVertex(0);
-		g.addVertex(1);
-		g.addVertex(2);
-		g.addVertex(3);
-		g.addVertex(4);
+        g.addVertex(0);
+        g.addVertex(1);
+        g.addVertex(2);
+        g.addVertex(3);
+        g.addVertex(4);
 
-		g.addVertex(5);
-		g.addVertex(6);
-		g.addVertex(7);
-		g.addVertex(8);
+        g.addVertex(5);
+        g.addVertex(6);
+        g.addVertex(7);
+        g.addVertex(8);
 
-		g.addEdge(0, 1);
-		g.addEdge(0, 4);
-		g.addEdge(1, 2);
-		g.addEdge(1, 3);
-		g.addEdge(1, 5);
-		g.addEdge(2, 3);
-		g.addEdge(2, 4);
-		g.addEdge(3, 4);
+        g.addEdge(0, 1);
+        g.addEdge(0, 4);
+        g.addEdge(1, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 5);
+        g.addEdge(2, 3);
+        g.addEdge(2, 4);
+        g.addEdge(3, 4);
 
-		g.addEdge(5, 6);
-		g.addEdge(5, 7);
-		g.addEdge(5, 8);
-		g.addEdge(6, 7);
-		g.addEdge(7, 8);
+        g.addEdge(5, 6);
+        g.addEdge(5, 7);
+        g.addEdge(5, 8);
+        g.addEdge(6, 7);
+        g.addEdge(7, 8);
 
-		// add self-loops
-		g.addEdge(4, 4);
-		g.addEdge(7, 7);
+        // add self-loops
+        g.addEdge(4, 4);
+        g.addEdge(7, 7);
 
-		UndirectedModularityMeasurer<Integer, DefaultWeightedEdge> measurer = new UndirectedModularityMeasurer<>(g);
+        UndirectedModularityMeasurer<Integer, DefaultWeightedEdge> measurer =
+            new UndirectedModularityMeasurer<>(g);
 
-		List<Set<Integer>> partitions = List.of(Set.of(0, 1, 2, 3, 4), Set.of(5, 6, 7, 8));
-		double mod = measurer.modularity(partitions);
+        List<Set<Integer>> partitions = List.of(Set.of(0, 1, 2, 3, 4), Set.of(5, 6, 7, 8));
+        double mod = measurer.modularity(partitions);
 
-		assertEquals(mod, 0.42444444444444446, 1e-6);
+        assertEquals(mod, 0.42444444444444446, 1e-6);
 
-	}
+    }
 
 }
