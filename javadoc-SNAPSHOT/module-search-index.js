432e3a66c6560d74c95d4ca6a20cf12dcdb23b11
jsichi <jsichi@gmail.com>
2026-05-28 15:46:54 +0000

    Deploying to gh-pages from @ jgrapht/jgrapht@da98049f4fafd8482fd31b477c40cfb6f282c7d9 🚀

--- javadoc-SNAPSHOT/module-search-index.js
+++ javadoc-SNAPSHOT/module-search-index.js
@@ -1,1 +1,1 @@
-moduleSearchIndex = [{"l":"org.jgrapht.core"},{"l":"org.jgrapht.demo"},{"l":"org.jgrapht.ext"},{"l":"org.jgrapht.guava"},{"l":"org.jgrapht.io"},{"l":"org.jgrapht.opt"},{"l":"org.jgrapht.unimi.dsi"}];updateSearchResults();
+moduleSearchIndex = [{"l":"org.jgrapht.core"},{"l":"org.jgrapht.demo"},{"l":"org.jgrapht.ext"},{"l":"org.jgrapht.guava"},{"l":"org.jgrapht.io"},{"l":"org.jgrapht.opt"},{"l":"org.jgrapht.osm"},{"l":"org.jgrapht.unimi.dsi"}];updateSearchResults();
